35b17b2ddad563d0e52fa2dac1874f8ee150fb63
Doug Gibson <email@dgibson.net>
2023-02-07 16:48:42 -0500

    Installed storybook 6.x

---
+++ packages/styles/.storybook/main.cjs
@@ -0,0 +1,20 @@
+module.exports = {
+  "stories": [
+    "../src/**/*.stories.mdx",
+    "../src/**/*.stories.@(js|jsx|ts|tsx)"
+  ],
+  "addons": [
+    "@storybook/addon-essentials",
+    "@whitespace/storybook-addon-html",
+    "@storybook/addon-interactions",
+    "@storybook/addon-links",
+  ],
+  "framework": "@storybook/html",
+  "core": {
+    "builder": "@storybook/builder-vite"
+  },
+  "features": {
+    "storyStoreV7": true
+  }
+  
+}
---
+++ packages/styles/.storybook/preview.cjs
@@ -0,0 +1,16 @@
+export const parameters = {
+  actions: { argTypesRegex: "^on[A-Z].*" },
+  controls: {
+    matchers: {
+      color: /(background|color)$/i,
+      date: /Date$/,
+    },
+  },
+  html: {
+    prettier: {
+      tabWidth: 4,
+      useTabs: false,
+      htmlWhitespaceSensitivity: "strict",
+    },
+  },
+};
