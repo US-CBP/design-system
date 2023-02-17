acae7b75626fb864fd26cfcab445a332ae9b3a87
Doug Gibson <email@dgibson.net>
2023-02-17 15:29:17 -0500

    Removed font awesome 6 from dependencies and build

--- packages/styles/vite.config.js
+++ packages/styles/vite.config.js
@@ -1,7 +1,7 @@
 // vite.config.js
 import { resolve } from 'path'
 import { defineConfig, normalizePath } from 'vite'
-import { viteStaticCopy } from 'vite-plugin-static-copy'
+//import { viteStaticCopy } from 'vite-plugin-static-copy'
 
 export default defineConfig({
   server: {
@@ -10,16 +10,7 @@ export default defineConfig({
   preview: {
     port: 8080,
   },
-  plugins: [
-    viteStaticCopy({
-      targets: [
-        {
-          src: '../../node_modules/@fortawesome/fontawesome-free/svgs/*',
-          dest: 'assets/icons'
-        }
-      ]
-    })
-  ],
+  plugins: [],
   build: {
     lib: {
       // Could also be a dictionary or array of multiple entry points
