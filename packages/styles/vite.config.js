3366e1c56194700a4887c68fa94e83548da73290
Danny Kim <dannyk3941@gmail.com>
2023-03-29 15:51:04 -0400

    chore: delete previous card scss and create its own folder, unminify scss and add base card style

--- packages/styles/vite.config.js
+++ packages/styles/vite.config.js
@@ -11,6 +11,7 @@ export default defineConfig({
   },
   plugins: [],
   build: {
+    minify: false,
     lib: {
       // Could also be a dictionary or array of multiple entry points
       entry: resolve(__dirname, 'src/index.js'),
