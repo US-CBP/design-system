51d50a1b0b8e0e30d56b0a1a1e3963c69832f237
Danny Kim <dannyk3941@gmail.com>
2023-03-31 08:24:03 -0400

    chore: clean up cssin expand component

--- packages/styles/src/components/expand/expand.stories.js
+++ packages/styles/src/components/expand/expand.stories.js
@@ -1,61 +1,61 @@
 export default {
   title: 'Patterns/Expand',
-  argType: {},
+  parameters: {
+    layout: 'centered'
+  },
+  argTypes: {
+    title: {
+      name: 'Expand Title',
+      description: 'Title of the Expand component to display',
+      type: { name: 'string', required: true }
+    },
+  },
+  // decorators: [
+  //   (Story) => (
+  //     `<div class="demo-container" style="margin: 12rem">
+  //       ${Story().outerHTML || Story()}
+  //     </div>`
+  //   )
+  // ]
 };
 
-const Template = () => {
+const ExpandTemplate = ({ title }) => {
   return `
     <div class="cbp-expand">
       <div class="cbp-expand__title">
-        <button>
-          <i class="fas fa-caret-down"></i>
-        </button>
-        <span>
-          This is a question about where frogs live.
-        </span>
+        <button><i class="fas fa-caret-down"></i></button>
+        <span>${title}</span>
       </div>
       <div class="cbp-expand__content">
-        <ul class="cbp-expand__list">
-          <li>There are different species of frogs and some make great pets!</li>
-          <li>Frogs can be bought at any pet store really. Here's a link to one <a href="http://petstore.com"
-              class="cbp-text-link" target="_blank" rel="noopener noreferrer">petstore.com</a></li>
-        </ul>
-      </div>
-    </div>
-    <div class="cbp-expand">
-      <div class="cbp-expand__title">
-        <button>
-          <i class="fas fa-caret-down"></i>
-        </button>
-        <span>
-          This is a question about where frogs live.
-        </span>
-      </div>
-      <div class="cbp-expand__content">
-        <ul class="cbp-expand__list">
-          <li>Frogs can be bought at any pet store really. Here's a link to one <a href="http://petstore.com"
-              target="_blank" rel="noopener noreferrer">petstore.com</a></li>
-        </ul>
-      </div>
-    </div>
-    <div class="cbp-expand">
-      <div class="cbp-expand__title">
-        <button>
-          <i class="fas fa-caret-down"></i>
-        </button>
-        <span>
-          Some facts about dinosaurs and their eating habits that you should teach your kids.
-        </span>
-      </div>
-      <div class="cbp-expand__content">
-        <ul class="cbp-expand__list">
-          <li>Frogs can be bought at any pet store really. Here's a link to one <a href="http://petstore.com"
-              target="_blank" rel="noopener noreferrer">petstore.com</a></li>
-        </ul>
+        <p class="cbp-text-body">There are different species of frogs and some make great pets!</p>
+        <p class="cbp-text-body">Frogs can be bought at any pet store really. Here's a link to one <a href="http://petstore.com" class="cbp-text-link" target="_blank" rel="noopener noreferrer">petstore.com</a></p>
       </div>
     </div>
   `;
 };
 
-export const Expand = Template.bind({});
-Expand.args = {};
+const ExpandLongTitleTemplate = ({ title }) => {
+  return `
+    <div class="cbp-expand" style="width: 513px">
+      <div class="cbp-expand__title">
+        <button><i class="fas fa-caret-down"></i></button>
+        <span>${title}</span>
+      </div>
+      <div class="cbp-expand__content">
+        <p class="cbp-text-body">There are different species of frogs and some make great pets!</p>
+        <p class="cbp-text-body">Frogs can be bought at any pet store really. Here's a link to one <a href="http://petstore.com" class="cbp-text-link" target="_blank" rel="noopener noreferrer">petstore.com</a></p>
+      </div>
+    </div>
+  `
+}
+
+export const Expand = ExpandTemplate.bind({});
+Expand.args = {
+  title: 'This is a question about where frogs live.'
+};
+
+export const ExpandLongTitle = ExpandLongTitleTemplate.bind({});
+ExpandLongTitle.args = {
+  title: 'Some facts about dinosaurs and their eating habits that you should teach your kids.'
+}
+ExpandLongTitle.storyName = 'Expand w/ Long Title';
