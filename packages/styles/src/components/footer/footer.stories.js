822a6b7423561a980566341f8c348526b2f931a3
Danny Kim <dannyk3941@gmail.com>
2023-04-12 15:57:14 -0400

    chore: add external links and fix negative margin for ntas logo

--- packages/styles/src/components/footer/footer.stories.js
+++ packages/styles/src/components/footer/footer.stories.js
@@ -47,13 +47,13 @@ const ExternalTemplate = () => {
       <section>
         <h6 class="cbp-footer__heading">Contact CBP</h6>
         <ul class="cbp-external-footer__social">
-          <li><a href="#"><i class="fab fa-twitter"></i></a></li>
-          <li><a href="#"><i class="fab fa-facebook"></i></a></li>
-          <li><a href="#"><i class="fab fa-youtube"></i></a></li>
-          <li><a href="#"><i class="fab fa-flickr"></i></a></li>
-          <li><a href="#"><i class="fab fa-instagram"></i></a></li>
-          <li><a href="#"><i class="fab fa-linkedin"></i></a></li>
-          <li><a href="#"><i class="fas fa-envelope"></i></a></li>
+          <li><a target="_blank" rel="noopener noreferrer" href="https://facebook.com/CBPgov"><i class="fab fa-facebook"></i></a></li>
+          <li><a target="_blank" rel="noopener noreferrer" href="https://twitter.com/CBP"><i class="fab fa-twitter"></i></a></li>
+          <li><a target="_blank" rel="noopener noreferrer" href="https://www.youtube.com/@customsborderprotect"><i class="fab fa-youtube"></i></a></li>
+          <li><a target="_blank" rel="noopener noreferrer" href="https://www.flickr.com/people/cbpphotos/"><i class="fab fa-flickr"></i></a></li>
+          <li><a target="_blank" rel="noopener noreferrer" href="https://instagram.com/CBPgov"><i class="fab fa-instagram"></i></a></li>
+          <li><a target="_blank" rel="noopener noreferrer" href="https://www.linkedin.com/company/customs-and-border-protection"><i class="fab fa-linkedin"></i></a></li>
+          <li><a target="_blank" rel="noopener noreferrer" href="https://public.govdelivery.com/accounts/USDHSCBP/subscriber/new"><i class="fas fa-envelope"></i></a></li>
         </ul>
       </section>
       <div class="cbp-footer__info">
@@ -65,21 +65,23 @@ const ExternalTemplate = () => {
           <p class="cbp-footer__text">An official website of the <br><a href="https://www.dhs.gov/" class="cbp-font-italic"><i class="fas fa-external-link-alt"></i>U.S. Department of Homeland Security</a></p>
         </div>
         <ul class="cbp-external-footer__links">
-          <li><a target="_blank" rel="noopener noreferrer" href="https://www.cbp.gov/site-policy-notices/accessibility"><i class="fas fa-arrow-right"></i>Accessibility</a></li>
-          <li><a target="_blank" rel="noopener noreferrer" href="https://www.cbp.gov/newsroom/accountability-and-transparency"><i class="fas fa-arrow-right"></i>Accountability</a></li>
-          <li><a target="_blank" rel="noopener noreferrer" href="https://www.dhs.gov/operational-and-support-components"><i class="fas fa-arrow-right"></i>DHS Components</a></li>
-          <li><a target="_blank" rel="noopener noreferrer" href="https://www.cbp.gov/site-policy-notices/foia"><i class="fas fa-arrow-right"></i>FOIA</a></li>
-          <li><a target="_blank" rel="noopener noreferrer" href="https://www.cbp.gov/newsroom/publications/forms"><i class="fas fa-arrow-right"></i>Forms</a></li>
-          <li><a target="_blank" rel="noopener noreferrer" href="https://www.oig.dhs.gov/taxonomy/term/1"><i class="fas fa-arrow-right"></i>Inspector General</a></li>
-          <li><a target="_blank" rel="noopener noreferrer" href="https://www.cbp.gov/about/eeo-diversity/no-fear-act"><i class="fas fa-arrow-right"></i>No FEAR Act</a></li>
-          <li><a target="_blank" rel="noopener noreferrer" href="https://www.cbp.gov/site-policy-notices/privacy-policy"><i class="fas fa-arrow-right"></i>Privacy</a></li>
-          <li><a target="_blank" rel="noopener noreferrer" href="https://www.cbp.gov/site-policy-notices"><i class="fas fa-arrow-right"></i>Site Policies</a></li>
-          <li><a target="_blank" rel="noopener noreferrer" href="https://www.whitehouse.gov/"><i class="fas fa-arrow-right"></i>The White House</a></li>
-          <li><a target="_blank" rel="noopener noreferrer" href="https://www.usa.gov/"><i class="fas fa-arrow-right"></i>USA.gov</a></li>
+          <li><a href="https://www.cbp.gov/contact"><i class="fas fa-arrow-right"></i>CBP Contact Information</a></li>
+          <li><a href="https://www.cbp.gov/site-policy-notices/accessibility"><i class="fas fa-arrow-right"></i>Accessibility</a></li>
+          <li><a href="https://www.cbp.gov/newsroom/publications/performance-accountability-financial"><i class="fas fa-arrow-right"></i>Accountability</a></li>
+          <li><a href="https://www.dhs.gov/operational-and-support-components"><i class="fas fa-arrow-right"></i>DHS Components</a></li>
+          <li><a href="https://www.cbp.gov/site-policy-notices/foia"><i class="fas fa-arrow-right"></i>FOIA</a></li>
+          <li><a href="https://www.cbp.gov/newsroom/publications/forms"><i class="fas fa-arrow-right"></i>Forms</a></li>
+          <li><a href="https://www.oig.dhs.gov/"><i class="fas fa-arrow-right"></i>Inspector General</a></li>
+          <li><a href="https://www.cbp.gov/about/eeo-diversity/no-fear-act"><i class="fas fa-arrow-right"></i>No FEAR Act</a></li>
+          <li><a href="https://www.cbp.gov/site-policy-notices/privacy-policy"><i class="fas fa-arrow-right"></i>Privacy</a></li>
+          <li><a href="https://www.cbp.gov/site-policy-notices"><i class="fas fa-arrow-right"></i>Site Policies</a></li>
+          <li><a target="_blank" rel="noopener noreferrer" href="https://www.whitehouse.gov/"><i class="fas fa-external-link-alt"></i>The White House</a></li>
+          <li><a href="https://www.cbp.gov/document/directives/vulnerability-disclosure-program-policy-and-rules-engagement"><i class="fas fa-arrow-right"></i>Vulnerability Disclosure Program</a></li>
+          <li><a href="https://www.usa.gov/"><i class="fas fa-external-link-alt"></i></i>USA.gov</a></li>
         </ul>
         <div class="cbp-external-footer__ntas">
           <img src="assets/images/NTAS_LOGO.svg" alt="National Terrorism Advisory System" height="84" width="200" />
-          <a href="#"><i class="fas fa-external-link-alt"></i>Learn More</a>
+          <a href="https://www.dhs.gov/ntas/advisory/national-terrorism-advisory-system-bulletin-november-30-2022"><i class="fas fa-external-link-alt"></i>Learn More</a>
         </div>
       </div>
     </footer>
