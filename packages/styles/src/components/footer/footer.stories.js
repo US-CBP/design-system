5f7993c79f1df1a1d82199668355e4e2b6dff88a
Danny Kim <dannyk3941@gmail.com>
2023-04-12 14:48:59 -0400

    chore: change class names to distinguish external footer and delete unnecessary css/html

--- packages/styles/src/components/footer/footer.stories.js
+++ packages/styles/src/components/footer/footer.stories.js
@@ -8,32 +8,34 @@ export default {
 const InternalTemplate = () => {
   return `
     <footer class="cbp-footer">
-      <ul class="cbp-footer__links">
-        <li><a href="#">App Overview</a></li>
-        <li><a href="#">Trainings</a></li>
-        <li><a href="#">FAQ's</a></li>
-        <li><a href="#">Release Notes</a></li>
-      </ul>
+      <nav>
+        <ul>
+          <li><a href="#">App Overview</a></li>
+          <li><a href="#">Trainings</a></li>
+          <li><a href="#">FAQ's</a></li>
+          <li><a href="#">Release Notes</a></li>
+        </ul>
+      </nav>
       <div class="cbp-footer__info">
-        <div>
-          <h6><i class="fas fa-headset"></i> Application Support</h6>
-          <p class="cbp-font-italic">This application is maintained by The Office of Information Technology: TASPD.</p>
-          <ul class="cbp-contact-list">
+        <section class="cbp-footer__support">
+          <h6 class="cbp-footer__heading"><i class="fas fa-headset"></i> Application Support</h6>
+          <p class="cbp-footer__text">This application is maintained by The Office of Information Technology: TASPD.</p>
+          <ul>
             <li>Having an issue?</li>
             <li><span>Email:</span>&nbsp;<a href="#">application-support@cbp.dhs.gov</a></li>
             <li><span>CBP Helpdesk:</span>&nbsp;(555) 555-5555</li>
           </ul>
-        </div>
-        <div class="cbp-footer__sec">
-          <img src="../assets/images/cbp-icon/SECURITY ICON - LES FOUO.svg" alt="security icon">
+        </section>
+        <div class="cbp-footer__security">
           <div>
-            <h6>Infosec Classification: FOUO</h6>
-            <p>This page contains information, in whole or part, that is marked FOR OFFFICIAL USE ONLY (FOUO).</p>
-            <a href="#"><i class="fas fa-external-link-alt"></i>&nbsp;Learn More About InfoSec</a>
+            <img src="../assets/images/cbp-icon/SECURITY ICON - LES FOUO.svg" alt="security icon">
+            <div>
+              <h6 class="cbp-footer__heading">Infosec Classification: FOUO</h6>
+              <p class="cbp-footer__text">This page contains information, in whole or part, that is marked FOR OFFFICIAL USE ONLY (FOUO). <a href="#"><i class="fas fa-external-link-alt"></i>Learn More About InfoSec</a></p>
+            </div>
           </div>
+          <p class="cbp-footer__text">This page contains information, in whole or part, that is marked FOR OFFFICIAL USE ONLY (FOUO). <a href="#"><i class="fas fa-external-link-alt"></i>Learn More About InfoSec</a></p>
         </div>
-        <p class="cbp-margin-top-3x">This page contains information, in whole or part, that is marked FOR OFFFICIAL USE ONLY (FOUO).</p>
-        <a href="#"><i class="fas fa-external-link-alt"></i>&nbsp;Learn More About InfoSec</a>
       </div>
     </footer>
   `;
@@ -41,10 +43,10 @@ const InternalTemplate = () => {
 
 const ExternalTemplate = () => {
   return `
-    <footer class="cbp-footer-external">
-      <div>
-        <p>Contact CBP</p>
-        <ul class="cbp-footer__social-links">
+    <footer class="cbp-external-footer">
+      <section>
+        <h6 class="cbp-footer__heading">Contact CBP</h6>
+        <ul class="cbp-external-footer__social">
           <li><a href="#"><i class="fab fa-twitter"></i></a></li>
           <li><a href="#"><i class="fab fa-facebook"></i></a></li>
           <li><a href="#"><i class="fab fa-youtube"></i></a></li>
@@ -53,29 +55,29 @@ const ExternalTemplate = () => {
           <li><a href="#"><i class="fab fa-linkedin"></i></a></li>
           <li><a href="#"><i class="fas fa-envelope"></i></a></li>
         </ul>
-      </div>
+      </section>
       <div class="cbp-footer__info">
         <div>
-          <div class="cbp-footer__seal">
+          <div class="cbp-external-footer__seal">
             <img src="assets/images/DHS_SEAL.svg" alt="U.S. Department of Homeland Security" height="44" width="44" />
             <span>CBP.gov</span>
           </div>
-          <p>An official website of the <br><a href="https://www.dhs.gov/" class="cbp-font-italic"><i class="fas fa-external-link-alt"></i>U.S. Department of Homeland Security</a></p>
+          <p class="cbp-footer__text">An official website of the <br><a href="https://www.dhs.gov/" class="cbp-font-italic"><i class="fas fa-external-link-alt"></i>U.S. Department of Homeland Security</a></p>
         </div>
-        <ul class="cbp-footer__external-links">
-          <li><a href="#"><i class="fas fa-arrow-right"></i>Accessibility</a></li>
-          <li><a href="#"><i class="fas fa-arrow-right"></i>Accountability</a></li>
-          <li><a href="#"><i class="fas fa-arrow-right"></i>DHS Components</a></li>
-          <li><a href="#"><i class="fas fa-arrow-right"></i>FOIA</a></li>
-          <li><a href="#"><i class="fas fa-arrow-right"></i>Forms</a></li>
-          <li><a href="#"><i class="fas fa-arrow-right"></i>Inspector General</a></li>
-          <li><a href="#"><i class="fas fa-arrow-right"></i>No FEAR Act</a></li>
-          <li><a href="#"><i class="fas fa-arrow-right"></i>Privacy</a></li>
-          <li><a href="#"><i class="fas fa-arrow-right"></i>Site Policies</a></li>
-          <li><a href="#"><i class="fas fa-arrow-right"></i>The White House</a></li>
-          <li><a href="#"><i class="fas fa-arrow-right"></i>USA.gov</a></li>
+        <ul class="cbp-external-footer__links">
+          <li><a target="_blank" rel="noopener noreferrer" href="https://www.cbp.gov/site-policy-notices/accessibility"><i class="fas fa-arrow-right"></i>Accessibility</a></li>
+          <li><a target="_blank" rel="noopener noreferrer" href="https://www.cbp.gov/newsroom/accountability-and-transparency"><i class="fas fa-arrow-right"></i>Accountability</a></li>
+          <li><a target="_blank" rel="noopener noreferrer" href="https://www.dhs.gov/operational-and-support-components"><i class="fas fa-arrow-right"></i>DHS Components</a></li>
+          <li><a target="_blank" rel="noopener noreferrer" href="https://www.cbp.gov/site-policy-notices/foia"><i class="fas fa-arrow-right"></i>FOIA</a></li>
+          <li><a target="_blank" rel="noopener noreferrer" href="https://www.cbp.gov/newsroom/publications/forms"><i class="fas fa-arrow-right"></i>Forms</a></li>
+          <li><a target="_blank" rel="noopener noreferrer" href="https://www.oig.dhs.gov/taxonomy/term/1"><i class="fas fa-arrow-right"></i>Inspector General</a></li>
+          <li><a target="_blank" rel="noopener noreferrer" href="https://www.cbp.gov/about/eeo-diversity/no-fear-act"><i class="fas fa-arrow-right"></i>No FEAR Act</a></li>
+          <li><a target="_blank" rel="noopener noreferrer" href="https://www.cbp.gov/site-policy-notices/privacy-policy"><i class="fas fa-arrow-right"></i>Privacy</a></li>
+          <li><a target="_blank" rel="noopener noreferrer" href="https://www.cbp.gov/site-policy-notices"><i class="fas fa-arrow-right"></i>Site Policies</a></li>
+          <li><a target="_blank" rel="noopener noreferrer" href="https://www.whitehouse.gov/"><i class="fas fa-arrow-right"></i>The White House</a></li>
+          <li><a target="_blank" rel="noopener noreferrer" href="https://www.usa.gov/"><i class="fas fa-arrow-right"></i>USA.gov</a></li>
         </ul>
-        <div class="cbp-footer__ntas">
+        <div class="cbp-external-footer__ntas">
           <img src="assets/images/NTAS_LOGO.svg" alt="National Terrorism Advisory System" height="84" width="200" />
           <a href="#"><i class="fas fa-external-link-alt"></i>Learn More</a>
         </div>
