e0325f3baa9499d750e2a7cbb8d3f87816a8ae61
Doug Gibson <email@dgibson.net>
2023-04-12 16:54:18 -0400

    Add chip interactivity

---
+++ packages/styles/src/components/chip/chip.js
@@ -0,0 +1,36 @@
+class Chip {
+  constructor(chip) {
+    this.chip = chip;
+
+    this.chip.addEventListener('click', (e) => {
+      this.handleClick(e);
+    });
+  }
+
+  handleClick(e) {
+    // In case of nested elements, get the button they belong to (closest() is self-referencing)
+    const clickedButton = e.target.closest('button');
+
+    const previousState = clickedButton.hasAttribute('aria-pressed') &&
+      clickedButton.getAttribute('aria-pressed') === 'true'
+      ? true
+      : false;
+
+    // Toggle active class
+    clickedButton.setAttribute('aria-pressed', (!previousState).toString());
+
+    // Emit a custom event so that the developer can listen to the group rather than individual buttons
+    const toggleEvent = new CustomEvent('chipClick', {
+      detail: {
+        chip: this.chip,
+        label: this.chip.innerText,
+        value: this.chip.value,
+        active: !previousState,
+        nativeEvent: e,
+      },
+    });
+    this.chip.dispatchEvent(toggleEvent);
+  }
+}
+
+export default Chip;
