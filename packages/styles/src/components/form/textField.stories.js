8ad3d4159ea89e9cbdee8a966312e6c1b95f3659
Danny Kim <dannyk3941@gmail.com>
2023-05-01 14:20:34 -0400

    chore: move inputs that do not have attached controls or buttons together

--- packages/styles/src/components/form/textField.stories.js
+++ packages/styles/src/components/form/textField.stories.js
@@ -1,19 +1,73 @@
 export default {
-  title: 'Patterns/Form/Text Field Components'
+  title: 'Patterns/Form/Inputs',
+  parameters: {
+    layout: 'centered',
+    html: {
+      root: '.cbp-form'
+    }
+  },
+  argTypes: {
+    label: {
+      name: 'Input Label',
+      control: { type: 'text' }
+    },
+    labelFor: {
+      name: 'Label `for` Attribute',
+      control: { type: 'text' }
+    },
+    inputDescription: {
+      name: 'Input Description',
+      control: { type: 'text' }
+    },
+    errorMessage: {
+      name: 'Error Message',
+      control: { type: 'text' }
+    },
+  },
+  decorators: [
+    (Story, context) => `
+      <form class="cbp-form" style="width: 512px;">
+        <label for=${context.args.labelFor} class="cbp-form__label">${context.args.label}</label>
+        <p class="cbp-form__description">${context.args.inputDescription}</p>
+        <p class="cbp-form__description cbp-form__description--error" hidden="true"><i class="fas fa-exclamation-triangle"></i>${context.args.errorMessage}</p>
+        ${Story().outerHTML || Story()}
+      </form>
+    `
+  ]
 };
 
-const Template = () => {
-  return `
-    <div class="cbp-form-wrapper">
-      <label for="firstName" class="cbp-form__label">First Name</label>
-      <span class="cbp-form__description">
-        Required. <span id="char-count-2" data-char-counter="true">250</span>/250 Characters remaining.
-      </span>
-      <span class="cbp-form__description cbp-form__description--error" hidden><i class="fas fa-exclamation-triangle"></i>&nbsp;This field is required.</span>
-      <input type="text" class="cbp-form__input" name="firstName" id="firstName" maxlength="250" oninput="countChar('char-count-2', event)" placeholder="Enter first name">
-    </div> 
+const Template = ({ labelFor }) => `<input type="text" class="cbp-form__input" name="firstName" id=${labelFor} placeholder="Enter first name."> `
+
+const TextAreaTemplate = ({ labelFor }) => `<textarea class="cbp-form__textarea" name="description" id=${labelFor} cols="30" rows="10" placeholder="Enter a description"></textarea>`
+
+const NumericFieldTemplate = ({ labelFor }) => (
   `
-}
+    <div class="cbp-form__number">
+      <input type="number" name="numeric" id=${labelFor} placeholder="Enter Number of Fish"></input>
+    </div>
+  `
+)
 
 export const TextField = Template.bind({});
-TextField.args = {};
+TextField.args = {
+  label: 'First Name',
+  labelFor: 'firstName',
+  inputDescription: 'Required. 250/250 Characters remaining.',
+  errorMessage: 'This field is required.'
+};
+
+export const TextArea = TextAreaTemplate.bind({});
+TextArea.args = {
+  label: 'Task Description',
+  labelFor: 'description',
+  inputDescription: 'Required. 500/500 Characters remaining.',
+  errorMessage: 'You must enter a description.'
+}
+
+export const NumericField = NumericFieldTemplate.bind({});
+NumericField.args = {
+  label: 'Number of Fish',
+  labelFor: 'numberOfFish',
+  inputDescription: 'Required.',
+  errorMessage: 'This field is required.'
+}
