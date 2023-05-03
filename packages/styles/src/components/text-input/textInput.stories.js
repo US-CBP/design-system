a9f24c9b4f4c0f220d3629a0b08356c8bc2e43f5
Danny Kim <dannyk3941@gmail.com>
2023-05-03 15:36:50 -0400

    chore: add disabled, required, and readonly attribute controls

--- packages/styles/src/components/text-input/textInput.stories.js
+++ packages/styles/src/components/text-input/textInput.stories.js
@@ -34,7 +34,17 @@ export default {
     },
     disabled: {
       name: '`disabled` Attribute',
-      description: 'When the `disabled` attribute is present, it makes the element not mutable, focusable or submitted with the form.',
+      description: 'When the `disabled` attribute is present on the input element, it makes the element not mutable, focusable or submitted with the form.',
+      control: { type: 'boolean' }
+    },
+    required: {
+      name: '`required` Attribute',
+      description: 'Indicates that the user must specify a value for the input before submission.',
+      control: { type: 'boolean' }
+    },
+    readonly: {
+      name: '`readonly` Attribute',
+      description: 'Makes the input element not mutable, meaning the user can not edit the control.',
       control: { type: 'boolean' }
     }
   },
@@ -42,8 +52,8 @@ export default {
     (Story, context) => `
       <form class="cbp-form">
         <label for=${context.args.labelFor} class="cbp-input__label">${context.args.label}</label>
-        <p class="cbp-input__description">${context.args.inputDescription}</p>
-        <p class="cbp-input__description cbp-input__description--error" hidden><i class="fas fa-exclamation-triangle"></i>${context.args.errorMessage}</p>
+        <p class="cbp-input__description" ${!context.args.required ? '' : 'hidden'}>${context.args.inputDescription}</p>
+        <p class="cbp-input__description ${context.args.required ? 'cbp-input__description--error': ''}" ${context.args.required ? '' : 'hidden'}><i class="fas fa-exclamation-triangle"></i>${context.args.errorMessage}</p>
         ${Story().outerHTML || Story()}
       </form>
     `
@@ -67,62 +77,75 @@ const tagsExample = () => `
   </div>
 `
 
-const TextfieldTemplate = ({ labelFor, inputType, inputName, disabled }) => `<input type=${inputType} class="cbp-input" name=${inputName} id=${labelFor} ${disabled ? 'disabled' : ''} placeholder="Enter first name."> `
+const TextfieldTemplate = ({ labelFor, inputType, inputName, disabled, required, readonly }) => (
+  `\
+    <input
+      type=${inputType}
+      class="cbp-input"
+      name=${inputName}
+      id=${labelFor}
+      ${disabled ? 'disabled' : ''}
+      ${required ? 'required' : ''}
+      ${readonly ? 'readonly' : ''}
+      placeholder="Enter first name."
+    /> 
+  `
+) 
 
 // '\' Removes line break shown in Storybook HTML preview
-const PasswordFieldTemplate = ({ inputName, labelFor, disabled }) => (
+const PasswordFieldTemplate = ({ inputName, labelFor, disabled, required, readonly }) => (
   `\
     <div class="cbp-input-group">
-      <input class="cbp-input" type="password" name=${inputName} id=${labelFor} ${disabled ? 'disabled' : ''} placeholder="Enter password" />
-      <button class="cbp-btn-square cbp-btn__secondary" type="button" aria-label="show password" ${disabled ? 'disabled' : ''}><i class="fas fa-eye-slash"></i></button>
+      <input class="cbp-input" type="password" name=${inputName} id=${labelFor} ${disabled ? 'disabled' : ''} ${required ? 'required' : ''} ${readonly ? 'readonly' : ''} placeholder="Enter password" />
+      <button class="cbp-btn-square cbp-btn__secondary" type="button" aria-label="show password" ${disabled || readonly ? 'disabled' : ''}><i class="fas fa-eye-slash"></i></button>
     </div>  
   `
 )
 
-const NumericCounterFieldTemplate = ({ labelFor, inputName }) => (
+const NumericCounterFieldTemplate = ({ labelFor, inputName, disabled, required, readonly }) => (
   `\
     <div class="cbp-input__numeric-counter" id="number-counter">
-      <input class="cbp-input" type="number" name=${inputName} id=${labelFor} placeholder="Enter Number of Fish"></input>
-      <button class="cbp-btn-square cbp-btn__secondary" type="button" id="decrement" aria-label="decrement">
+      <input class="cbp-input" type="number" name=${inputName} id=${labelFor}  ${disabled ? 'disabled' : ''} ${required ? 'required' : ''} ${readonly ? 'readonly' : ''} placeholder="Enter Number of Fish"></input>
+      <button class="cbp-btn-square cbp-btn__secondary" type="button" id="decrement"  ${disabled || readonly ? 'disabled' : ''} aria-label="decrement">
         <i class="fas fa-minus"></i>
       </button>
-      <button class="cbp-btn-square cbp-btn__secondary" type="button" id="increment" aria-label="increment">
+      <button class="cbp-btn-square cbp-btn__secondary" type="button" id="increment"  ${disabled || readonly ? 'disabled' : ''} aria-label="increment">
         <i class="fas fa-plus"></i>
       </button>
     </div>
   `
 )
 
-const NumericSwitchTemplate = ({ labelFor, inputName }) => (
+const NumericSwitchTemplate = ({ labelFor, inputName, disabled, required, readonly }) => (
   `\
     <div class="cbp-input__numeric-switch">
       <div>
-        <input class="cbp-input" type="number" name=${inputName} id=${labelFor} placeholder="Enter Weight"></input>
+        <input class="cbp-input" type="number" name=${inputName} id=${labelFor} ${disabled ? 'disabled' : ''} ${required ? 'required' : ''} ${readonly ? 'readonly' : ''} placeholder="Enter Weight"></input>
       </div>
       <div class="cbp-btn--segment">
-        <button type="button" value="lbs">LBS</button>
-        <button type="button" value="kg">KG</button>
+        <button type="button" value="lbs" ${disabled || readonly ? 'disabled' : ''}>LBS</button>
+        <button type="button" value="kg" ${disabled || readonly ? 'disabled' : ''}>KG</button>
       </div>
     </div>
   `
 )
 
-const TextfieldButtonGroupTemplate = ({ square, labelFor, inputType, tags, inputName, disabled }) => (
+const TextfieldButtonGroupTemplate = ({ square, labelFor, inputType, tags, inputName, disabled, buttonLabel, required }) => (
   `\
     <div class="cbp-input-group">
-      <input class="cbp-input" type=${inputType} name=${inputName} id=${labelFor} ${disabled ? 'disabled' : ''} placeholder="Add Person" />
-      <button class="${square ? 'cbp-btn-square' : 'cbp-btn'} cbp-btn__secondary" type="button" ${disabled ? 'disabled' : ''}>${square ? '<i class="fas fa-plus"></i>' : 'Add Person'}</button>
+      <input class="cbp-input" type=${inputType} name=${inputName} id=${labelFor} ${disabled ? 'disabled' : ''} ${required ? 'required' : ''} ${readonly ? 'readonly' : ''} placeholder="Add Person" />
+      <button class="${square ? 'cbp-btn-square' : 'cbp-btn'} ${required ? 'cbp-btn__danger' : 'cbp-btn__secondary'}" type="button" ${disabled || readonly ? 'disabled' : ''}>${square ? '<i class="fas fa-plus"></i>' : buttonLabel}</button>
     </div>
     ${tags ? tagsExample() : ''}
   `
 )
 
 // TODO: Predictive search functionality
-const SearchFieldTemplate = ({ labelFor, inputName }) => (
+const SearchFieldTemplate = ({ labelFor, inputName, disabled, required }) => (
   `\
     <div class="cbp-input-group">
-      <input class="cbp-input" type="search" name=${inputName} id=${labelFor} placeholder="Enter Search Criteria"></input>
-      <button type="button" class="cbp-btn-square cbp-btn__secondary" aria-label="search">
+      <input class="cbp-input" type="search" name=${inputName} id=${labelFor} ${disabled ? 'disabled' : ''} ${required ? 'required' : ''} ${readonly ? 'readonly' : ''} placeholder="Enter Search Criteria"></input>
+      <button type="button" class="cbp-btn-square ${required ? 'cbp-btn__danger' : 'cbp-btn__secondary'}" ${disabled || readonly ? 'disabled' : ''} aria-label="search">
         <i class="fas fa-search"></i>
       </button>
     </div>
@@ -152,6 +175,7 @@ PasswordField.args = {
   label: 'Password',
   inputDescription: 'Required',
   labelFor: 'password',
+  inputName: 'password',
   errorMessage: 'This password does not match our records.'
 }
 
@@ -180,7 +204,8 @@ TextfieldButtonGroup.args = {
   inputType: 'text',
   inputName: 'addPerson',
   errorMessage: 'This field is required.',
-  tags: false
+  buttonLabel: 'Add Person',
+  chips: false
 };
 TextfieldButtonGroup.argTypes = {
   square: {
@@ -192,6 +217,17 @@ TextfieldButtonGroup.argTypes = {
     description: 'Types listed here are the available options for text types, refer to documentation for all other types.',
     control: { type: 'select' },
     options: ['text', 'number', 'email', 'tel', 'url']
+  },
+  buttonLabel: {
+    name: 'Button Label',
+    description: 'Text inside the button element. **_Not available with square button variant_**',
+    control: { type: 'select' },
+    options: ['text', 'number', 'email', 'tel', 'url']
+  },
+  tags: {
+    name: 'Show Chips?',
+    description: 'Displays Chips that can be part of the text input group.',
+    control: { type: 'boolean' },
   }
 }
 TextfieldButtonGroup.storyName = 'Textfield w/ Button';
--- packages/styles/src/components/textarea/textarea.stories.js
+++ packages/styles/src/components/textarea/textarea.stories.js
@@ -23,10 +23,20 @@ export default {
       name: 'Error Message',
       control: { type: 'text' }
     },
+    inputName: {
+      name: 'Input `name` Attribute',
+      description: 'Name of the form control in the input element. Submitted with the form as part of a name/value pair.',
+      control: { type: 'text' }
+    },
+    disabled: {
+      name: '`disabled` Attribute',
+      description: 'When the `disabled` attribute is present on the input element, it makes the element not mutable, focusable or submitted with the form.',
+      control: { type: 'boolean' }
+    }
   },
   decorators: [
     (Story, context) => `
-      <form class="cbp-form" style="width: 512px;">
+      <form class="cbp-form">
         <label for=${context.args.labelFor} class="cbp-input__label">${context.args.label}</label>
         <p class="cbp-input__description">${context.args.inputDescription}</p>
         <p class="cbp-input__description cbp-input__description--error" hidden="true"><i class="fas fa-exclamation-triangle"></i>${context.args.errorMessage}</p>
@@ -36,12 +46,13 @@ export default {
   ]
 };
 
-const TextAreaTemplate = ({ labelFor }) => `<textarea class="cbp-input" name="description" id=${labelFor} cols="30" rows="10" placeholder="Enter a description"></textarea>`
+const TextAreaTemplate = ({ labelFor, inputName, disabled }) => `<textarea class="cbp-input" name=${inputName} id=${labelFor} ${disabled ? 'disabled' : ''} placeholder="Enter a description"></textarea>`
 
 export const TextArea = TextAreaTemplate.bind({});
 TextArea.args = {
   label: 'Task Description',
   labelFor: 'description',
   inputDescription: 'Required. 500/500 Characters remaining.',
+  inputName: 'description',
   errorMessage: 'You must enter a description.'
 }
