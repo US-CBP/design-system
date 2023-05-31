a12ea4c0afd3f43f8cdfcb972a987d0d92f349e9
Danny Kim <dannyk3941@gmail.com>
2023-05-31 16:49:15 -0400

    chore: clean up css and story controls

--- packages/vanilla/src/components/select/select.stories.js
+++ packages/vanilla/src/components/select/select.stories.js
@@ -5,6 +5,48 @@ export default {
       root: '.cbp-form'
     }
   },
+  argTypes: {
+    label: {
+      name: 'Label Element',
+      description: 'Represents a caption for the `<select>` element in the user interface.',
+      control: { type: 'text' }
+    },
+    inputDescription: {
+      name: 'Input Description',
+      description: 'Instructions or supplementary information regarding the `<select>` element.',
+      control: { type: 'text' }
+    },
+    labelFor: {
+      name: 'Label `for` Attribute',
+      description: 'Indicates the form element that this `<label>` describes and has the value which is the `id` of the `<select>` element it relates to.',
+      control: { type: 'text' }
+    },
+    formControlName: {
+      name: 'Input `name` Attribute',
+      description: 'Name of the form control in the input element. Submitted with the form as part of a name/value pair.',
+      control: { type: 'text' }
+    },
+    placeholderOption: {
+      name: 'Placeholder Option Text',
+      description: 'Text of the placeholder option.',
+      control: { type: 'text' }
+    },
+    errorMessage: {
+      name: 'Error Message',
+      description: 'A message in the input description that a problem has occurred.',
+      control: { type: 'text' }
+    },
+    optionsObj: {
+      name: 'Options Object',
+      description: 'Sets the label of the `<option>` and the `value` attribute.',
+      control: { type: 'object' }
+    },
+    disabled: {
+      name: '`disabled` Attribute',
+      description: 'Attribute that indicates that the user cannot interact with the control.',
+      control: { type: 'boolean' }
+    }
+  },
   decorators: [
     (Story, context) => `
       <form class="cbp-form">
@@ -17,9 +59,9 @@ export default {
   ]
 }
 
-const Template = ({ labelFor, formControlName, placeholderOption, optionsObj: { option1, option2, option3, option4, option5} }) => (
+const Template = ({ labelFor, formControlName, placeholderOption, disabled, optionsObj: { option1, option2, option3, option4, option5} }) => (
   `
-    <select name=${formControlName} id=${labelFor}>
+    <select class="cbp-input__select" name=${formControlName} id=${labelFor} ${disabled ? 'disabled' : ''}>
       <option value="" selected>${placeholderOption}</option>
       <option value=${option1.value}>${option1.label}</option>
       <option value=${option2.value}>${option2.label}</option>
@@ -60,5 +102,5 @@ Select.args = {
       label: 'Port of Zoolily'
     },
   },
-  required: false,
+  disabled: false
 };
