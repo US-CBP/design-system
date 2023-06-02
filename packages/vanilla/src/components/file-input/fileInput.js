1fcc3e01e3882246e8571d6ea80b51e84d9c0a32
Doug Gibson <email@dgibson.net>
2023-06-02 16:56:16 -0400

    Removed console.logs

--- packages/vanilla/src/components/file-input/fileInput.js
+++ packages/vanilla/src/components/file-input/fileInput.js
@@ -2,17 +2,17 @@
 
 const fileUtil = {
   createUpload: (key, name, progressValue, progressMax, files) => {
-    const fileProgress = document.createElement("div");
-    const fileName = document.createElement("span");
-    const cancelIcon = document.createElement("i");
-    const cancelBtn = document.createElement("button");
-    const progressBar = document.createElement("progress");
+    const fileProgress = document.createElement('div');
+    const fileName = document.createElement('span');
+    const cancelIcon = document.createElement('i');
+    const cancelBtn = document.createElement('button');
+    const progressBar = document.createElement('progress');
 
-    fileProgress.className = "cbp-file-input__file";
+    fileProgress.className = 'cbp-file-input__file';
     fileProgress.id = `${key}_${name}`;
 
     fileName.innerText = name;
-    cancelIcon.className = "fa fa-times";
+    cancelIcon.className = 'fa fa-times';
     cancelBtn.appendChild(cancelIcon);
 
     cancelBtn.onclick = (e) => {
@@ -29,65 +29,58 @@ const fileUtil = {
     return fileProgress;
   },
   createErrorUpload: (key, name) => {
-    const errorUpload = document.createElement("div");
-    const fileContainer = document.createElement("div");
-    const errorFile = document.createElement("span");
-    const cancelBtn = document.createElement("button");
-    const divider = document.createElement("hr");
-    const errorMessage = document.createElement("span");
-    const errorIcon = document.createElement("i");
-    const cancelIcon = document.createElement("i");
+    const errorUpload = document.createElement('div');
+    const fileContainer = document.createElement('div');
+    const errorFile = document.createElement('span');
+    const cancelBtn = document.createElement('button');
+    const divider = document.createElement('hr');
+    const errorMessage = document.createElement('span');
+    const errorIcon = document.createElement('i');
+    const cancelIcon = document.createElement('i');
 
     errorUpload.id = `${key}_${name}`;
     errorFile.innerText = name;
     cancelIcon.className = 'fa fa-times';
-    cancelBtn.appendChild = cancelIcon
+    cancelBtn.appendChild = cancelIcon;
 
-    errorIcon.className = "fas fa-exclamation-triangle";
+    errorIcon.className = 'fas fa-exclamation-triangle';
     errorMessage.appendChild(cancelIcon);
-    errorMessage.innerText = 'File size exceeds the limit.'
+    errorMessage.innerText = 'File size exceeds the limit.';
 
     fileContainer.appendChild(errorFile);
     fileContainer.appendChild(cancelBtn);
 
     errorUpload.appendChild(fileContainer);
     errorUpload.appendChild(divider);
-    errorUpload.appendChild(errorMessage)
+    errorUpload.appendChild(errorMessage);
 
     return errorUpload;
   },
   cancelUpload: (key, files, event) => {
     const list = new DataTransfer();
-    const fileArray = Object.entries(files)
+    const fileArray = Object.entries(files);
     const filteredList = fileArray.filter(([listkey, value]) => listkey != key);
-    const filteredFiles = filteredList.map(arr => arr[1]);
-    
-    filteredFiles.forEach(file => list.items.add(file));
+    const filteredFiles = filteredList.map((arr) => arr[1]);
 
-    const upload = event.target.closest('.cbp-file-input__file')
+    filteredFiles.forEach((file) => list.items.add(file));
+
+    const upload = event.target.closest('.cbp-file-input__file');
     upload.remove();
-    // Reset the input's value to no files selected (this won't work with multiple file uploads)
-    //event.srcElement.value="";
-    this.input.value=undefined;
-    
-    console.log("File Removed: ", this.input, event.srcElement.value, event.srcElement.files);
   },
 };
-
 
 class FileInput {
   constructor(element) {
     this.formWrapper = element.closest('.cbp-form-wrapper');
     this.fileinput = element;
-    this.input = element.querySelector("input[type=file]");
+    this.input = element.querySelector('input[type=file]');
 
     this.handleInput(this.input, this.reader);
   }
 
   handleInput(input, reader) {
-    input.addEventListener("change", (e) => {
+    input.addEventListener('change', (e) => {
       this.handleReader(e.target.files);
-      console.log('Change event: ', e, "Files: ", e.srcElement.files.length)
     });
   }
 
@@ -95,43 +88,57 @@ class FileInput {
     for (const [key, value] of Object.entries(files)) {
       const reader = new FileReader();
 
-      reader.addEventListener("loadstart", (e) => this.handleEvent(e, key, value, files));
-      reader.addEventListener("load", (e) => this.handleEvent(e, key, value, files));
-      reader.addEventListener("loadend", (e) => this.handleEvent(e, key, value, files));
-      reader.addEventListener("progress", (e) => this.handleEvent(e, key, value, files));
-      reader.addEventListener("error", (e) => this.handleEvent(e, key, value, files));
-      reader.addEventListener("abort", this.handleEvent);
+      reader.addEventListener('loadstart', (e) =>
+        this.handleEvent(e, key, value, files)
+      );
+      reader.addEventListener('load', (e) =>
+        this.handleEvent(e, key, value, files)
+      );
+      reader.addEventListener('loadend', (e) =>
+        this.handleEvent(e, key, value, files)
+      );
+      reader.addEventListener('progress', (e) =>
+        this.handleEvent(e, key, value, files)
+      );
+      reader.addEventListener('error', (e) =>
+        this.handleEvent(e, key, value, files)
+      );
+      reader.addEventListener('abort', this.handleEvent);
 
       reader.readAsDataURL(value);
     }
   }
 
   handleEvent(event, key, value, files) {
-    if (event.type === "loadstart") {
-      console.log('loadstart process');
-      const upload = fileUtil.createUpload(key, value.name, event.loaded, event.total, files);
-      this.fileinput.insertAdjacentElement("afterend", upload);
+    if (event.type === 'loadstart') {
+      const upload = fileUtil.createUpload(
+        key,
+        value.name,
+        event.loaded,
+        event.total,
+        files
+      );
+      this.fileinput.insertAdjacentElement('afterend', upload);
     }
 
-    if (event.type === "progress") {
+    if (event.type === 'progress') {
       // update progress bar value
       const upload = document.getElementById(`${key}_${value.name}`);
-      const progress = upload.querySelector("progress");
-      console.log('progress process')
-      progress.setAttribute("value", event.loaded);
+      const progress = upload.querySelector('progress');
+      progress.setAttribute('value', event.loaded);
     }
 
     if (event.type === 'loadend') {
       const upload = document.getElementById(`${key}_${value.name}`);
-      const progress = upload.querySelector("progress");
+      const progress = upload.querySelector('progress');
 
       // Hide progress bar once file has finished reading
       progress.hidden = true;
     }
 
-    if (event.type === "error") {
+    if (event.type === 'error') {
       const upload = fileUtil.createErrorLoad(key, value.name);
-      this.fileinput.insertAdjacentElement("afterend", upload);
+      this.fileinput.insertAdjacentElement('afterend', upload);
     }
   }
 }
