505655b7783fad6ca5a9b6aa485316aea418e7fc
Danny Kim <dannyk3941@gmail.com>
2023-06-20 17:24:03 -0400

    fix: banner card styles

---
+++ packages/vanilla/src/components/card/bannerCard.stories.js
@@ -0,0 +1,45 @@
+export default {
+  title: 'Patterns/Card',
+  parameters: {
+    layout: 'centered'
+  },
+  argTypes: {
+    title: {
+      name: 'Title',
+      control: 'text'
+    },
+    modifier: {
+      name: 'Modifier',
+      control: 'select',
+      options: [
+        '',
+        'cbp-card__banner--info',  
+        'cbp-card__banner--success', 
+        'cbp-card__banner--danger'
+      ]
+    }
+  }
+};
+
+const BannerCardTemplate = ({ title, modifier }) => {
+  return `
+    <div class="cbp-card__banner ${modifier}">
+      <h4 class="cbp-card__banner-title">
+        <i class="fas fa-info"></i>
+        ${title}
+      </h4>
+      <div class="cbp-card__banner-content">
+        <p class="cbp-text-body">
+          Here is an example of some supplementary text for this purely informational card
+        </p>
+      </div>
+    </div>
+  `;
+};
+
+export const BannerCard = BannerCardTemplate.bind({});
+BannerCard.args = {
+  title: 'Banner Card',
+  modifier: ''
+};
+
