d990fdd18f2d79e69ab5d291dbe36715297481a5
Danny Kim <dannyk3941@gmail.com>
2023-07-06 13:22:45 -0400

    chore: add all icons to stories and add margin bottom utility class

--- packages/vanilla/src/components/card/bannerCard.stories.js
+++ packages/vanilla/src/components/card/bannerCard.stories.js
@@ -19,21 +19,28 @@ export default {
         'success', 
         'danger'
       ]
-    }
+    },
+    showIcon: {
+      name: 'Show Icon',
+      description: 'Show an icon in the title',
+      control: 'boolean'
+    },
   }
 };
 
+const icons = {
+  default: '<i class="fas fa-info"></i>',
+  info: '<i class="fas fa-info-circle"></i>',  
+  success: '<i class="fas fa-check"></i>',
+  danger: '<i class="fas fa-exclamation-triangle"></i>'
+}
 
-
-const BannerCardTemplate = ({ title, variant }) => {
+const BannerCardTemplate = ({ title, variant, showIcon }) => {
   return `
     <div class="cbp-card__banner ${variant != 'default' ? `cbp-card__banner--${variant}` : ''}">
-      <h4 class="cbp-card__banner-title">
-        <i class="fas fa-info"></i>
-        ${title}
-      </h4>
+      <h4 class="cbp-card__banner-title">${showIcon ? icons[variant] : ''}${title}</h4>
       <div class="cbp-card__banner-content">
-        <p class="cbp-text-body">
+        <p class="cbp-text-body cbp-margin-bottom-0">
           Here is an example of some supplementary text for this purely informational card
         </p>
       </div>
