ad35568bc0519566554e82158c72b8745ab36266
Danny Kim <dannyk3941@gmail.com>
2023-07-13 13:55:05 -0400

    feat: add layouts folder and initial about page markdown file

---
+++ packages/ux-guidelines/content/about.md
@@ -0,0 +1,6 @@
+---
+layout: layout.njk
+title: About
+---
+
+# What is the CBP Design System?
--- packages/ux-guidelines/eleventy.config.js
+++ packages/ux-guidelines/eleventy.config.js
@@ -3,6 +3,7 @@ module.exports = function(eleventyConfig) {
   return {
     dir: {
       input: "content",
+      includes: "../_includes",
       output: "_site"
     }
   }
