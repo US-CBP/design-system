ce40a4a5a6e6534609ca5465201ce554a42b6b5c
Danny Kim <dannyk3941@gmail.com>
2023-07-14 11:45:09 -0400

    fix: add path to css in base layout file and add foundations folder

--- packages/ux-guidelines/eleventy.config.js
+++ packages/ux-guidelines/eleventy.config.js
@@ -1,4 +1,8 @@
 module.exports = function(eleventyConfig) {
+  eleventyConfig.addPassthroughCopy({
+    "../../node_modules/@cbpds/vanilla/dist/style.css": "assets/cbpds-vanilla.css"
+  })
+  
   // Return your Object options:
   return {
     dir: {
