20d19bd0af78e79fe8a22c67b8d9a0216ede5f1c
Danny Kim <dannyk3941@gmail.com>
2023-08-02 09:33:10 -0400

    chore: delete unnecessary files/folders

--- packages/react-components/vite.config.js
+++ packages/react-components/vite.config.js
@@ -4,4 +4,26 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    lib: {
+      // Could also be a dictionary or array of multiple entry points
+      entry: resolve(__dirname, 'src/main.js'),
+      name: 'MyLib',
+      // the proper extensions will be added
+      fileName: 'my-lib',
+    },
+    rollupOptions: {
+      // make sure to externalize deps that shouldn't be bundled
+      // into your library
+      external: ['react'],
+      output: {
+        // Provide global variables to use in the UMD build
+        // for externalized deps
+        globals: {
+          react: 'React',
+          'react-dom': 'ReactDOM'
+        },
+      },
+    },
+  },
 })
