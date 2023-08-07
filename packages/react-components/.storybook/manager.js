b216a873506e12883cc6c6b52aec8954d444a723
dannyk3941 <52807859+dannyk3941@users.noreply.github.com>
2023-08-07 12:18:26 -0400

    Add react package (#52)
    * feat: add react-components workspace with vite
    
    * feat: add react-components storybook
    
    * chore: delete unnecessary files/folders
    
    * feat: add initial button component and library mode vite config
    
    * chore: run npm audit fix
    
    * chore: remove tools from toolbar, add styling addon and set theme
    
    * chore: clean up readme
    
    * chore: add react-components to package table in root readme
    
    * fix: change class to className in markup

---
+++ packages/react-components/.storybook/manager.js
@@ -0,0 +1,19 @@
+import { addons } from '@storybook/addons';
+import { create } from '@storybook/theming'
+
+import cbpLogo from '@cbpds/vanilla/assets/images/CBP_SEAL.svg'
+
+const theme =  create({
+  base: 'light',
+  // Brand assets
+  brandTitle: 'CBP Design System | React',
+  brandImage: cbpLogo,
+  brandTarget: '_self',
+});
+
+addons.setConfig({
+  theme,
+  toolbar: {
+    zoom: { hidden: true },
+  },
+});
