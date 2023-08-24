c676b5ba196a0a21baa6c0ebddfbff769efbe1c5
Danny Kim <dannyk3941@gmail.com>
2023-08-24 12:30:44 -0400

    chore: add right padding to select element css for caret and more css for pagination pattern

--- packages/vanilla/src/components/pagination/pagination.stories.js
+++ packages/vanilla/src/components/pagination/pagination.stories.js
@@ -5,19 +5,33 @@ export default {
 const Template = () => {
   return `
     <div class="cbp-pagination">
-      <button class="cbp-pagination__prev" type="button" aria-label="Previous Page">
-        <i class="fas fa-chevron-left"></i>
-      </button>
-      <select aria-label="Select page">
-        <option value="1">Pg. 1 of 1000</option>
-        <option value="2">Pg. 2 of 1000</option>
-        <option value="3">Pg. 3 of 1000</option>
-        <option value="4">Pg. 4 of 1000</option>
-        <option value="5">Pg. 5 of 1000</option>
-      </select>
-      <button class="cbp-pagination__next" type="button" aria-label="Next Page">
-        <i class="fas fa-chevron-right"></i>
-      </button>
+      <div class="cbp-pagination__results">
+        <select aria-label="Select result amount">
+          <option value="">10/Page</option>
+          <option value="25">25 Per Page</option>
+          <option value="50">50 Per Page</option>
+          <option value="100">100 Per Page</option>
+          <option value="100">9000 Per Page</option>
+        </select>
+        <span class="cbp-pagination__item-count">
+          1-10 of 103 Items
+        </span>
+      </div>
+      <div class="cbp-pagination__pages">
+        <button class="cbp-pagination__prev" type="button" aria-label="Previous Page">
+          <i class="fas fa-chevron-left"></i>
+        </button>
+        <select aria-label="Select page">
+          <option value="1">Pg. 1 of 1000</option>
+          <option value="2">Pg. 2 of 1000</option>
+          <option value="3">Pg. 3 of 1000</option>
+          <option value="4">Pg. 4 of 1000</option>
+          <option value="5">Pg. 5 of 1000</option>
+        </select>
+        <button class="cbp-pagination__next" type="button" aria-label="Next Page">
+          <i class="fas fa-chevron-right"></i>
+        </button>
+      </div>
     </div>
   `
 }
