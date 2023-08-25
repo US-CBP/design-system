54da62607701f5b79fa615d088aa11a96d081051
dannyk3941 <52807859+dannyk3941@users.noreply.github.com>
2023-08-25 14:15:39 -0400

    Feat/pagination css (#59)
    * feat: initial pagination pattern
    
    * chore: add right padding to select element css for caret and more css for pagination pattern
    
    * chore: update css and class names for pagination pattern
    
    * fix: review comments and doc
    
    * fix: caret direction in select

---
+++ packages/vanilla/src/components/pagination/pagination.stories.js
@@ -0,0 +1,40 @@
+export default {
+  title: 'Patterns/Pagination',
+};
+
+const Template = () => {
+  return `
+    <div class="cbp-pagination">
+      <div class="cbp-pagination__items">
+        <select aria-label="Items per page">
+          <option value="">10/Page</option>
+          <option value="25">25 Per Page</option>
+          <option value="50">50 Per Page</option>
+          <option value="100">100 Per Page</option>
+          <option value="100">9000 Per Page</option>
+        </select>
+        <span class="cbp-pagination__item-count">
+          1-10 of 103 Items
+        </span>
+      </div>
+      <div class="cbp-pagination__pages">
+        <button class="cbp-pagination__prev" type="button" aria-label="Previous Page">
+          <i class="fas fa-chevron-left"></i>
+        </button>
+        <select aria-label="Select Page">
+          <option value="1">Page 1 of 1000</option>
+          <option value="2">Page 2 of 1000</option>
+          <option value="3">Page 3 of 1000</option>
+          <option value="4">Page 4 of 1000</option>
+          <option value="5">Page 5 of 1000</option>
+        </select>
+        <button class="cbp-pagination__next" type="button" aria-label="Next Page">
+          <i class="fas fa-chevron-right"></i>
+        </button>
+      </div>
+    </div>
+  `;
+};
+
+export const Pagination = Template.bind({});
+Pagination.args = {};
