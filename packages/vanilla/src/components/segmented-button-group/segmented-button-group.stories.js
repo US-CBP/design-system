232548962d609399285a110297e26205b7aa4d57
Doug Gibson <email@dgibson.net>
2023-10-03 09:04:27 -0400

    Updates to square button and use in segmented buttons (#76)

--- packages/vanilla/src/components/segmented-button-group/segmented-button-group.stories.js
+++ packages/vanilla/src/components/segmented-button-group/segmented-button-group.stories.js
@@ -6,9 +6,9 @@ export default {
 const SingleSelectTemplate = () => {
   return `
     <div class="cbp-btn--segment" data-segmented-button-type="single">
-      <button type="button" value="sm">Small</button>
-      <button type="button" value="md">Medium</button>
-      <button type="button" value="lg">Large</button>
+      <button class="cbp-btn" type="button" value="sm">Small</button>
+      <button class="cbp-btn" type="button" value="md">Medium</button>
+      <button class="cbp-btn" type="button" value="lg">Large</button>
     </div>
   `;
 };
@@ -16,10 +16,10 @@ const SingleSelectTemplate = () => {
 const MultiSelectTemplate = () => {
   return `
     <div class="cbp-btn--segment" data-segmented-button-type="multi">
-      <button type="button" value="sauce">Sauce</button>
-      <button type="button" value="cheese">Cheese</button>
-      <button type="button" value="crust">Crust</button>
-      <button type="button" value="toppings">Toppings</button>
+      <button class="cbp-btn" type="button" value="sauce">Sauce</button>
+      <button class="cbp-btn" type="button" value="cheese">Cheese</button>
+      <button class="cbp-btn" type="button" value="crust">Crust</button>
+      <button class="cbp-btn" type="button" value="toppings">Toppings</button>
     </div>
   `;
 };
@@ -27,14 +27,14 @@ const MultiSelectTemplate = () => {
 const IconSelectTemplate = () => {
   return `
     <div class="cbp-btn--segment" data-segmented-button-type="multi">
-      <button type="button" value="bold" aria-label="bold"><i class="fas fa-bold"></i></button>
-      <button type="button" value="italic" aria-label="italic"><i class="fas fa-italic"></i></button>
-      <button type="button" value="underline" aria-label="underline"><i class="fas fa-underline"></i></button>
-      <button type="button" value="ordered list" aria-label="ordered list"><i class="fas fa-list-ol"></i></button>
-      <button type="button" value="list" aria-label="unordered list"><i class="fas fa-list"></i></button>
-      <button type="button" value="superscript" aria-label="superscript"><i class="fas fa-superscript"></i></button>
-      <button type="button" value="subscript" aria-label="subscript"><i class="fas fa-subscript"></i></button>
-      <button type="button" value="eye dropper" aria-label="eye dropper"><i class="fas fa-eye-dropper"></i></button>
+      <button class="cbp-btn-square" type="button" value="bold" aria-label="bold"><i class="fas fa-bold"></i></button>
+      <button class="cbp-btn-square" type="button" value="italic" aria-label="italic"><i class="fas fa-italic"></i></button>
+      <button class="cbp-btn-square" type="button" value="underline" aria-label="underline"><i class="fas fa-underline"></i></button>
+      <button class="cbp-btn-square" type="button" value="ordered list" aria-label="ordered list"><i class="fas fa-list-ol"></i></button>
+      <button class="cbp-btn-square" type="button" value="list" aria-label="unordered list"><i class="fas fa-list"></i></button>
+      <button class="cbp-btn-square" type="button" value="superscript" aria-label="superscript"><i class="fas fa-superscript"></i></button>
+      <button class="cbp-btn-square" type="button" value="subscript" aria-label="subscript"><i class="fas fa-subscript"></i></button>
+      <button class="cbp-btn-square" type="button" value="eye dropper" aria-label="eye dropper"><i class="fas fa-eye-dropper"></i></button>
     </div>
   `;
 };
