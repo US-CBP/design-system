319cc75de88c56f26a422bb84f868c57eb8a98bb
Doug Gibson <email@dgibson.net>
2023-10-04 12:56:02 -0400

    Add controls to segmented button stories

--- packages/vanilla/src/components/segmented-button-group/segmented-button-group.stories.js
+++ packages/vanilla/src/components/segmented-button-group/segmented-button-group.stories.js
@@ -1,32 +1,45 @@
 export default {
   title: 'Patterns/Segmented Button Group',
-  argTypes: {},
+  argTypes: {
+    selection: {
+      name: 'Selection',
+      description: 'Choose whether a single or multiple selections are allowed.',
+      control: 'radio',
+      options: ['single', 'multi']
+    },
+  },
 };
 
-const SingleSelectTemplate = () => {
+
+function generateButtons(buttons) {
+  let html=[];
+  buttons.forEach( ({ label, value, disabled }, i) => {
+    html=[...html, `<button type="button" class="cbp-btn" value="${value}" ${disabled==true ? 'disabled' : ''}>${label}</button>`];
+  });
+  return [...html].join("");
+};
+
+
+
+const SingleSelectTemplate = ( {buttons, selection} ) => {
   return `
-    <div class="cbp-btn--segment" data-segmented-button-type="single">
-      <button class="cbp-btn" type="button" value="sm">Small</button>
-      <button class="cbp-btn" type="button" value="md">Medium</button>
-      <button class="cbp-btn" type="button" value="lg">Large</button>
+    <div class="cbp-btn--segment" data-segmented-button-type="${selection}">
+      ${generateButtons(buttons)}
     </div>
   `;
 };
 
-const MultiSelectTemplate = () => {
+const MultiSelectTemplate = ({buttons, selection}) => {
   return `
-    <div class="cbp-btn--segment" data-segmented-button-type="multi">
-      <button class="cbp-btn" type="button" value="sauce">Sauce</button>
-      <button class="cbp-btn" type="button" value="cheese">Cheese</button>
-      <button class="cbp-btn" type="button" value="crust">Crust</button>
-      <button class="cbp-btn" type="button" value="toppings">Toppings</button>
+    <div class="cbp-btn--segment" data-segmented-button-type="${selection}">
+      ${generateButtons(buttons)}
     </div>
   `;
 };
 
-const IconSelectTemplate = () => {
+const IconSelectTemplate = ({buttons, selection}) => {
   return `
-    <div class="cbp-btn--segment" data-segmented-button-type="multi">
+    <div class="cbp-btn--segment" data-segmented-button-type="${selection}">
       <button class="cbp-btn-square" type="button" value="bold" aria-label="bold"><i class="fas fa-bold"></i></button>
       <button class="cbp-btn-square" type="button" value="italic" aria-label="italic"><i class="fas fa-italic"></i></button>
       <button class="cbp-btn-square" type="button" value="underline" aria-label="underline"><i class="fas fa-underline"></i></button>
@@ -40,10 +53,69 @@ const IconSelectTemplate = () => {
 };
 
 export const SingleSelect = SingleSelectTemplate.bind({});
-SingleSelect.args = {};
+SingleSelect.argTypes = {
+  buttons: {
+    name: 'Buttons',
+    description: 'Configure various aspects of the buttons within the segmented button group.',
+    control: 'object'
+  }
+}
+SingleSelect.args = {
+  buttons: [
+      {
+        label: "Small",
+        value: "sm",
+        disabled: false
+      },
+      {
+        label: "Medium",
+        value: "md",
+        disabled: false
+      },
+      {
+        label: "Large",
+        value: "lg",
+        disabled: false
+      }
+    ],
+  selection: "single"
+};
 
 export const MultiSelect = MultiSelectTemplate.bind({});
-MultiSelect.args = {};
+MultiSelect.argTypes = {
+  buttons: {
+    name: 'Buttons',
+    description: 'Configure various aspects of the buttons within the segmented button group.',
+    control: 'object'
+  }
+}
+MultiSelect.args = {
+  buttons: [
+    {
+      label: "Sauce",
+      value: "sauce",
+      disabled: false
+    },
+    {
+      label: "Cheese",
+      value: "cheese",
+      disabled: false
+    },
+    {
+      label: "Crust",
+      value: "crust",
+      disabled: false
+    },
+    {
+      label: "Toppings",
+      value: "toppings",
+      disabled: false
+    },
+  ],
+  selection: "multi"
+};
 
 export const IconSelect = IconSelectTemplate.bind({});
-IconSelect.args = {};
+IconSelect.args = {
+  selection: "multi"
+};
