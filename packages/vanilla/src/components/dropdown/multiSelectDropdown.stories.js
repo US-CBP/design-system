d8397bd4556d8136d0908a15947616d0d11d7a36
Doug Gibson <email@dgibson.net>
2023-10-04 10:19:50 -0400

    Fix dropdown issues mostly around e.target errors, updated chip in multi-select (#78)

--- packages/vanilla/src/components/dropdown/multiSelectDropdown.stories.js
+++ packages/vanilla/src/components/dropdown/multiSelectDropdown.stories.js
@@ -5,55 +5,53 @@ export default {
 
 const Template = () => {
   return `
-    <div class="cbp-form-wrapper">
-      <label for="pod2" class="cbp-form__label">Port(s) of Departure</label>
-      <span class="cbp-form__description">
-        Required.
-      </span>
-      <span class="cbp-form__description cbp-form__description--error" hidden><i
-          class="fas fa-exclamation-triangle"></i>&nbsp;This field is required.</span>
-      <div class="cbp-dropdown-wrapper" id="dropdown-demo-3">
-        <button class="cbp-dropdown__custom" id="ports-dropdown" data-toggle="dropdown">
-          <span class="cbp-dropdown__placeholder">Choose Ports</span>
-        </button>
-        <div class="cbp-dropdown" id="custom-dropdown-menu">
-          <label class="cbp-dropdown__item cbp-dropdown__item--multiselect">
-            <input type="checkbox" value="22" autocomplete="off">
-            Port of Balitmore
-          </label>
-          <label class="cbp-dropdown__item cbp-dropdown__item--multiselect">
-            <input type="checkbox" value="82" autocomplete="off">
-            Port of Newark
-          </label>
-          <label class="cbp-dropdown__item cbp-dropdown__item--multiselect">
-            <input type="checkbox" value="11" autocomplete="off">
-            Port of Washington
-          </label>
-          <label class="cbp-dropdown__item cbp-dropdown__item--multiselect">
-            <input type="checkbox" value="667" autocomplete="off">
-            Port of Zoolily
-          </label>
-          <label class="cbp-dropdown__item cbp-dropdown__item--multiselect">
-            <input type="checkbox" value="2" autocomplete="off">
-            Port of Boston
-          </label>
-          <label class="cbp-dropdown__item cbp-dropdown__item--multiselect">
-            <input type="checkbox" value="65" autocomplete="off">
-            Port of San Ysidro
-          </label>
-          <label class="cbp-dropdown__item cbp-dropdown__item--multiselect">
-            <input type="checkbox" value="87" autocomplete="off">
-            Port of San Diego
-          </label>
-          <label class="cbp-dropdown__item cbp-dropdown__item--multiselect">
-            <input type="checkbox" value="998" autocomplete="off">
-            Port of Rhode Island
-          </label>
-          <label class="cbp-dropdown__item cbp-dropdown__item--multiselect">
-            <input type="checkbox" value="423" autocomplete="off">
-            Port of Galveston
-          </label>
-        </div>
+    <label for="pod2" class="cbp-form__label">Port(s) of Departure</label>
+    <span class="cbp-form__description">
+      Required.
+    </span>
+    <span class="cbp-form__description cbp-form__description--error" hidden><i
+        class="fas fa-exclamation-triangle"></i>&nbsp;This field is required.</span>
+    <div class="cbp-dropdown__wrapper" id="dropdown-demo-3">
+      <button class="cbp-dropdown__custom" id="ports-dropdown" data-toggle="dropdown">
+        <span class="cbp-dropdown__placeholder">Choose Ports</span>
+      </button>
+      <div class="cbp-dropdown" id="custom-dropdown-menu">
+        <label class="cbp-dropdown__item cbp-dropdown__item--multiselect">
+          <input type="checkbox" value="22" autocomplete="off">
+          Port of Baltimore
+        </label>
+        <label class="cbp-dropdown__item cbp-dropdown__item--multiselect">
+          <input type="checkbox" value="82" autocomplete="off">
+          Port of Newark
+        </label>
+        <label class="cbp-dropdown__item cbp-dropdown__item--multiselect">
+          <input type="checkbox" value="11" autocomplete="off">
+          Port of Washington
+        </label>
+        <label class="cbp-dropdown__item cbp-dropdown__item--multiselect">
+          <input type="checkbox" value="667" autocomplete="off">
+          Port of Zoolily
+        </label>
+        <label class="cbp-dropdown__item cbp-dropdown__item--multiselect">
+          <input type="checkbox" value="2" autocomplete="off">
+          Port of Boston
+        </label>
+        <label class="cbp-dropdown__item cbp-dropdown__item--multiselect">
+          <input type="checkbox" value="65" autocomplete="off">
+          Port of San Ysidro
+        </label>
+        <label class="cbp-dropdown__item cbp-dropdown__item--multiselect">
+          <input type="checkbox" value="87" autocomplete="off">
+          Port of San Diego
+        </label>
+        <label class="cbp-dropdown__item cbp-dropdown__item--multiselect">
+          <input type="checkbox" value="998" autocomplete="off">
+          Port of Rhode Island
+        </label>
+        <label class="cbp-dropdown__item cbp-dropdown__item--multiselect">
+          <input type="checkbox" value="423" autocomplete="off">
+          Port of Galveston
+        </label>
       </div>
     </div>
   `;
