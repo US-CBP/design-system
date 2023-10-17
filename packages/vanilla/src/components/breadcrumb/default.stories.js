4a3b53f3caa2e54a2e4b3e46287766ea9a849554
Danny Kim <dannyk3941@gmail.com>
2023-10-17 10:33:57 -0400

    fix: cleanup default breadcrumb story and scss

--- packages/vanilla/src/components/breadcrumb/default.stories.js
+++ packages/vanilla/src/components/breadcrumb/default.stories.js
@@ -5,22 +5,19 @@ export default {
 
 const Template = () => {
   return `
-    <div>
-      <ul class="cbp-breadcrumb" data-breadcrumb="true">
-        <li class="cbp-breadcrumb__item">
-          <button class="cbp-breadcrumb__btn">
-            <i class="fas fa-home"></i>
-          </button>
-        </li>
-        <li class="cbp-breadcrumb__item">
-          <a href="#">Child Page</a>
-        </li>
-        <li class="cbp-breadcrumb__item">
-          <a href="#">Grandchild Page</a>
-        </li>
-      </ul>
-      <h1 class="cbp-lastcrumb">Primary Passengers Application</h1>
-    </div> 
+    <ul class="cbp-breadcrumb" data-breadcrumb="true">
+      <li class="cbp-breadcrumb__item">
+        <a href="/" class="cbp-breadcrumb__btn" aria-label="Home">
+          <i class="fas fa-home"></i>
+        </a>
+      </li>
+      <li class="cbp-breadcrumb__item">
+        <a href="#">Child Page</a>
+      </li>
+      <li class="cbp-breadcrumb__item">
+        <a href="#">Grandchild Page</a>
+      </li>
+    </ul>
   `;
 };
 
