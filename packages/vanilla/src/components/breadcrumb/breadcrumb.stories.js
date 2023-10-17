01e10b100513665379b3944620411ffe864ce23c
Danny Kim <dannyk3941@gmail.com>
2023-10-17 11:27:12 -0400

    chore: add menu to correct story

--- packages/vanilla/src/components/breadcrumb/breadcrumb.stories.js
+++ packages/vanilla/src/components/breadcrumb/breadcrumb.stories.js
@@ -15,42 +15,6 @@ const Template = () => {
         <a href="#">Child Page</a>
       </li>
       <li class="cbp-breadcrumb__item">
-      <div class="cbp-menu">
-        <button
-          type="button"
-          aria-haspopup="menu"
-          aria-controls="menu1"
-          aria-label="More Options"
-          id="menubutton1"
-          class="cbp-menu__control cbp-btn-square cbp-btn__secondary-ghost"
-          aria-expanded="false"
-        >
-          <i class="fas fa-ellipsis-v"></i>
-        </button>
-        <ul
-          role="menu"
-          id="menu1"
-          class="cbp-menu__menu cbp-menu--bottom-start"
-          aria-labelledby="menubutton1"
-        >
-          <li class="cbp-menu__item">
-            <a href="#" tabindex="-1">First Option</a>
-          </li>
-          <li class="cbp-menu__item">
-            <a href="#" tabindex="-1">Second Option</a>
-          </li>
-          <li class="cbp-menu__item">
-            <a href="#" tabindex="-1">Third Option</a>
-          </li>
-          <li class="cbp-menu__item">
-            <button class="cbp-menu__close-btn" tabindex="-1">
-              <i class="fas fa-times-circle"></i>Close
-            </button>
-          </li>
-        </ul>
-      </div>    
-      </li>
-      <li class="cbp-breadcrumb__item">
         <a href="#">Grandchild Page</a>
       </li>
     </ul>
@@ -64,6 +28,42 @@ const CondensedTemplate = () => {
         <a href="/" aria-label="Home">
           <i class="fas fa-home"></i>
         </a href="/">
+      </li>
+      <li class="cbp-breadcrumb__item">
+        <div class="cbp-menu">
+          <button
+            type="button"
+            aria-haspopup="menu"
+            aria-controls="menu1"
+            aria-label="More Options"
+            id="menubutton1"
+            class="cbp-menu__control cbp-btn-square cbp-btn__secondary-ghost"
+            aria-expanded="false"
+          >
+            <i class="fas fa-ellipsis-v"></i>
+          </button>
+          <ul
+            role="menu"
+            id="menu1"
+            class="cbp-menu__menu cbp-menu--bottom-start"
+            aria-labelledby="menubutton1"
+          >
+            <li class="cbp-menu__item">
+              <a href="#" tabindex="-1">First Option</a>
+            </li>
+            <li class="cbp-menu__item">
+              <a href="#" tabindex="-1">Second Option</a>
+            </li>
+            <li class="cbp-menu__item">
+              <a href="#" tabindex="-1">Third Option</a>
+            </li>
+            <li class="cbp-menu__item">
+              <button class="cbp-menu__close-btn" tabindex="-1">
+                <i class="fas fa-times-circle"></i>Close
+              </button>
+            </li>
+          </ul>
+        </div> 
       </li>
       <li class="cbp-breadcrumb__item">
         <a href="#">Grandchild Page</a>
