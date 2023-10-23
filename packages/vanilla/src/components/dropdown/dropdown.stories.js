8cecab9233841dea49d9fb669ea587019d59c35f
Doug Gibson <email@dgibson.net>
2023-10-23 13:20:58 -0400

    Bugfix/inputs 508 (#87)
    * Added aria-describedby to text inputs and textareas
    
    * Fix accessibility issues with dropdowns
    
    * Added `aria-describedby` and removed `multiple` references from file input
    
    * Removed instance of `multiple`

--- packages/vanilla/src/components/dropdown/dropdown.stories.js
+++ packages/vanilla/src/components/dropdown/dropdown.stories.js
@@ -6,14 +6,24 @@ export default {
 const Template = () => {
   return `
     <div class="cbp-input-pattern">
-      <label for="pod2" class="cbp-input__label">Variant Selector</label>
-      <div class="cbp-input__description cbp-form__description--error" hidden>
+      <label for="port-dropdown" class="cbp-input__label">Variant Selector</label>
+      <div class="cbp-input__description cbp-form__description--error" id="port-dropdown-description" hidden>
         <i class="fas fa-exclamation-triangle"></i> This field is required.
       </div>
       <div class="cbp-dropdown__wrapper" id="dropdown-demo-2">
-        <button class="cbp-dropdown__custom" id="port-dropdown" data-toggle="dropdown">
+        <button
+          class="cbp-dropdown__custom"
+          id="port-dropdown"
+          data-toggle="dropdown"
+          role="combobox"
+          aria-controls="custom-dropdown-menu"
+          aria-expanded="false"
+          aria-haspopup="listbox"
+          data-aria-describedby="port-dropdown-description"
+        >
           <span class="cbp-dropdown__placeholder">Choose Variant</span>
         </button>
+
         <div class="cbp-dropdown" id="custom-dropdown-menu">
           <a href="#" class="cbp-dropdown__item">Single Button Card</a>
           <a href="#" class="cbp-dropdown__item">Dual Button Card</a>
