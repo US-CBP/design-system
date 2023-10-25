963f33467450a92f41884df395af37037f033565
Doug Gibson <email@dgibson.net>
2023-10-25 10:30:28 -0400

    Install SASS plugin

--- packages/web-components/src/components.d.ts
+++ packages/web-components/src/components.d.ts
@@ -6,56 +6,32 @@
  */
 import { HTMLStencilElement, JSXBase } from "@stencil/core/internal";
 export namespace Components {
-    interface MyComponent {
-        /**
-          * The first name
-         */
-        "first": string;
-        /**
-          * The last name
-         */
-        "last": string;
-        /**
-          * The middle name
-         */
-        "middle": string;
+    interface CbpButton {
     }
 }
 declare global {
-    interface HTMLMyComponentElement extends Components.MyComponent, HTMLStencilElement {
+    interface HTMLCbpButtonElement extends Components.CbpButton, HTMLStencilElement {
     }
-    var HTMLMyComponentElement: {
-        prototype: HTMLMyComponentElement;
-        new (): HTMLMyComponentElement;
+    var HTMLCbpButtonElement: {
+        prototype: HTMLCbpButtonElement;
+        new (): HTMLCbpButtonElement;
     };
     interface HTMLElementTagNameMap {
-        "my-component": HTMLMyComponentElement;
+        "cbp-button": HTMLCbpButtonElement;
     }
 }
 declare namespace LocalJSX {
-    interface MyComponent {
-        /**
-          * The first name
-         */
-        "first"?: string;
-        /**
-          * The last name
-         */
-        "last"?: string;
-        /**
-          * The middle name
-         */
-        "middle"?: string;
+    interface CbpButton {
     }
     interface IntrinsicElements {
-        "my-component": MyComponent;
+        "cbp-button": CbpButton;
     }
 }
 export { LocalJSX as JSX };
 declare module "@stencil/core" {
     export namespace JSX {
         interface IntrinsicElements {
-            "my-component": LocalJSX.MyComponent & JSXBase.HTMLAttributes<HTMLMyComponentElement>;
+            "cbp-button": LocalJSX.CbpButton & JSXBase.HTMLAttributes<HTMLCbpButtonElement>;
         }
     }
 }
--- packages/web-components/stencil.config.ts
+++ packages/web-components/stencil.config.ts
@@ -1,4 +1,5 @@
 import { Config } from '@stencil/core';
+import { sass } from '@stencil/sass';
 
 export const config: Config = {
   namespace: 'web-components',
@@ -17,6 +18,9 @@ export const config: Config = {
       type: 'www',
       serviceWorker: null, // disable service workers
     },
+  ],
+  plugins: [
+    sass()
   ],
   testing: {
     browserHeadless: "new",
