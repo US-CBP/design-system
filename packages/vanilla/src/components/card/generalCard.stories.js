96af73e1b303ef7266ce5c3f8798da3914d4df39
dannyk3941 <52807859+dannyk3941@users.noreply.github.com>
2023-10-26 15:46:24 -0400

    Fix/general card (#88)
    * fix: remove small general card story
    
    * chore: add warning variant to general card
    
    * chore: add storybook control to show/hide icon in general card header
    
    * chore: add body text control in storybook
    
    * fix: card title single line for extra space issue

--- packages/vanilla/src/components/card/generalCard.stories.js
+++ packages/vanilla/src/components/card/generalCard.stories.js
@@ -16,51 +16,46 @@ export default {
       name: 'Modifier',
       description: 'Set the modifier class of the card',
       control: 'select',
-      options: ['default', 'info', 'success', 'danger']
+      options: ['default', 'info', 'success', 'danger', 'warning']
+    },
+    showIcon: {
+      name: 'Show Icon',
+      description: 'Show an icon in the title',
+      control: 'boolean'
+    },
+    bodyText: {
+      name: 'Body Text',
+      description: 'Set the body text of the card',
+      control: 'text'
     }
   }
 };
 
-const DefaultGeneralCardTemplate = ({ title, modifier }) => {
+const DefaultGeneralCardTemplate = ({ title, modifier, showIcon, bodyText }) => {
   return `
     <div class="wrapper" style="display: grid; grid-template-columns: 432px">
       <div class="cbp-card ${modifier != 'default' ? `cbp-card--${modifier}` : ''}">
         <div class="cbp-card__content">
           <div class="cbp-card__header">
-            <h4 class="cbp-card__title"><i class="fas fa-info-circle cbp-margin-right-2x"></i>${title}</h4>
+            <h4 class="cbp-card__title">${showIcon ? '<i class="fas fa-info-circle cbp-margin-right-2x"></i>' : ''}${title}</h4>
           </div>
-          <p class="cbp-text-body cbp-margin-top-4x">Here is an example of some supplementary text for this purely informational card</p>
+          <p class="cbp-text-body cbp-margin-top-4x">${bodyText}</p>
         </div>
       </div>
     </div>
   `;
 };
 
-const GeneralCardMediaTemplate = ({ title, modifier }) => {
+const GeneralCardMediaTemplate = ({ title, modifier, showIcon, bodyText }) => {
   return `
     <div class="wrapper" style="display: grid; grid-template-columns: 486px">
-      <div class="cbp-card ${modifier} cbp-display-flex">
+      <div class="cbp-card ${modifier != 'default' ? `cbp-card--${modifier}` : ''} cbp-display-flex">
         <img src="assets/images/profile-page/passenge-photo-v2.jpg" class="cbp-card__img-left" alt="portrait of a person" height="127" width="116">
         <div class="cbp-card__content">
           <div class="cbp-card__header">
-            <h4 class="cbp-card__title"><i class="fas fa-info-circle cbp-margin-right-2x"></i>${title}</h4>
+            <h4 class="cbp-card__title">${showIcon ? '<i class="fas fa-info-circle cbp-margin-right-2x"></i>' : ''}${title}</h4>
           </div>
-          <p class="cbp-text-body cbp-margin-top-4x">Here is an example of some supplementary text for this purely informational card</p>
-        </div>
-      </div>
-    </div>
-  `;
-};
-
-const SmallGeneralCardTemplate = ({ title, modifier }) => {
-  return `
-    <div class="wrapper" style="display: grid; grid-template-columns: 312px">
-      <div class="cbp-card ${modifier}">
-        <div class="cbp-card__content">
-          <div class="cbp-card__header">
-            <h4 class="cbp-card__title"><i class="fas fa-info-circle cbp-margin-right-2x"></i>${title}</h4>
-          </div>
-          <p class="cbp-text-body cbp-margin-top-4x">Here is an example of some supplementary text for this purely informational card</p>
+          <p class="cbp-text-body cbp-margin-top-4x">${bodyText}</p>
         </div>
       </div>
     </div>
@@ -70,20 +65,15 @@ const SmallGeneralCardTemplate = ({ title, modifier }) => {
 export const DefaultGeneralCard = DefaultGeneralCardTemplate.bind({});
 DefaultGeneralCard.args = {
   title: 'General Card',
-  modifier: 'default'
+  modifier: 'default',
+  bodyText: 'Here is an example of some supplementary text for this purely informational card'
 };
 DefaultGeneralCard.storyName = 'Default';
 
 export const GeneralCardMedia = GeneralCardMediaTemplate.bind({});
 GeneralCardMedia.args = {
   title: 'General Card',
-  modifier: 'default'
+  modifier: 'default',
+  bodyText: 'Here is an example of some supplementary text for this purely informational card'
 };
 GeneralCardMedia.storyName = 'With Media';
-
-export const SmallGeneralCard = SmallGeneralCardTemplate.bind({});
-SmallGeneralCard.args = {
-  title: 'General Card',
-  modifier: 'default'
-};
-SmallGeneralCard.storyName = 'Small Size';
