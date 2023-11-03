d0fe0b54ed572ae0edf23d8254c13e7f17537747
Doug Gibson <email@dgibson.net>
2023-11-03 11:08:06 -0400

    Added missing variants and anchor buttons to button component

--- packages/web-components/src/components/cbp-button/cbp-button.stories.tsx
+++ packages/web-components/src/components/cbp-button/cbp-button.stories.tsx
@@ -4,18 +4,41 @@ export default {
   argTypes: {
     label: {
       name: 'label (slotted)',
-      description: 'Label text in the `<button>` element and used to describe the action when clicked.',
+      description: 'Label text in the `button` element and used to describe the action when clicked.',
       control: 'text'
+    },
+    tag: {
+      description: 'Both buttons and anchors may be visually styled like buttons. Be sure to only specify the appropriate attributes for the selected tag.',
+      control: 'select',
+      options: [
+        'button',
+        'a',
+      ]
     },
     type: {
       description: 'The `type` attribute of the button. (not valid for anchors/links)',
-      defaultValue: 'button',
-      control: 'radio',
+      control: 'select',
       options: [
+        'button',
         'submit',
         'reset',
-        'button'
       ]
+    },
+    href: {
+      description: 'Specifies the `href` attribute for anchor buttons. Not valid on actual `button` tags.',
+      control: 'text'
+    },
+    rel: {
+      description: 'Specifies the `rel` attribute for anchor buttons. Not valid on actual `button` tags.',
+      control: 'text'
+    },
+    target: {
+      description: 'Specifies the `target` attribute for anchor buttons. Not valid on actual `button` tags.',
+      control: 'text'
+    },
+    download: {
+      description: 'Indicates whether the `download` attribute is placed on the rendered anchor tag as a browser hint indicating that the linked contents specified in the `href` should be downloaded rather than navigated to. Not valid on actual `button` tags.',
+      control: 'boolean'
     },
     fill: {
       description: 'Displays button fill in the overall button hierarchy. Available options are: `Solid`, `Outline` and `Ghost`.',
@@ -28,17 +51,18 @@ export default {
     },
     color: {
       description: '`primary` is generally the dominant action among their peer buttons, `secondary` is the most common to appear but is not the dominant action over its peers, `danger` can indicate an action that cannot easily be undone.',
-      control: 'radio',
+      control: 'select',
       options: [
         'primary',
         'secondary',
+        'tertiary',
         'danger'
       ]
     },
     variant: {
-      description: 'Choose button variant',
-      control: 'radio',
-      options: ['square', 'cta']
+      description: 'Variants includes a larger "Call to Action" and a square button for icons-only.',
+      control: 'select',
+      options: [ 'default', 'square', 'cta']
     },
     accessibilityText: {
       description: 'Accessibility text is applied as an `aria-label` and should be supplied when the button does not contain text or it is not sufficiently and uniquely descriptive.',
@@ -52,18 +76,30 @@ export default {
       description: 'Supports adding inline styles as an object of key-value pairs comprised of CSS properties and values. Values should reference design tokens when possible.',
       control: 'object'
     },
-    
+  },
+  args: {
+    label: 'Default',
+    tag: 'button',
+    type: 'button',
+    variant: 'default',
+    color: 'primary',
+    fill: 'solid',
   }
 };
 
-const Template = ({ label, type, fill, color, variant, accessibilityText, disabled, sx }) => { 
+const Template = ({ label, tag, type, href, rel, target, download, fill, color, variant, accessibilityText, disabled, sx }) => { 
   return ` 
       <cbp-app>
       <cbp-button
+        ${tag !== 'button' ? 'tag="'+tag+'"' : ''}
         type="${type}"
+        ${href ? 'href="'+href+'"' : ''}
+        ${rel ? 'rel="'+rel+'"' : ''}
+        ${target ? 'target="'+target+'"' : ''}
+        ${download ? 'download="'+download+'"' : ''}
         fill="${fill}"
         color="${color}"
-        ${variant ? 'variant="'+variant+'"' : ''}
+        ${variant !== 'default' ? 'variant="'+variant+'"' : ''}
         ${accessibilityText ? 'accessibility-text="'+accessibilityText+'"' : ''}
         ${disabled ? 'disabled="'+disabled+'"' : ''}
         ${sx ? 'sx='+JSON.stringify(sx) : ''}
@@ -75,10 +111,13 @@ const Template = ({ label, type, fill, color, variant, accessibilityText, disabl
 }
 
 export const Button = Template.bind({});
+/*
 Button.args = {
   label: 'Default',
+  tag: 'button',
   type: 'button',
   variant: 'default',
   color: 'primary',
   fill: 'solid',
 };
+*/
