eca904e9336f2811c39384591a88531167638736
Doug Gibson <email@dgibson.net>
2023-11-15 10:17:32 -0500

    Added logo file for universal header and copy command to build

--- packages/web-components/stencil.config.ts
+++ packages/web-components/stencil.config.ts
@@ -3,19 +3,29 @@ import { sass } from '@stencil/sass';
 
 export const config: Config = {
   namespace: 'cbp-web-components',
+  globalStyle: 'src/global/styles/index.scss',
   outputTargets: [
     {
       type: 'dist',
       esmLoaderPath: 'loader',
+      copy: [
+        { src: '../assets', dest: 'dist/assets' }
+       ],
     },
     {
       type: 'dist-custom-elements',
+      copy: [
+        { src: '../assets', dest: 'dist/assets' }
+      ],
     },
     {
       type: 'docs-readme',
     },
     {
       type: 'www',
+      copy: [
+        { src: '../assets', dest: './assets' }
+      ],
       serviceWorker: null, // disable service workers
     },
   ],
