e28ef5488c9a66bb1b3ef1b2022bc33f1a268e70
Danny Kim <dannyk3941@gmail.com>
2023-11-16 13:26:22 -0500

    fix: remove npm package from web-components

--- packages/react-components/components/stencil-generated/index.ts
+++ packages/react-components/components/stencil-generated/index.ts
@@ -10,3 +10,5 @@ import type { JSX } from '@cbpds/web-components';
 export const CbpApp = /*@__PURE__*/createReactComponent<JSX.CbpApp, HTMLCbpAppElement>('cbp-app');
 export const CbpButton = /*@__PURE__*/createReactComponent<JSX.CbpButton, HTMLCbpButtonElement>('cbp-button');
 export const CbpCard = /*@__PURE__*/createReactComponent<JSX.CbpCard, HTMLCbpCardElement>('cbp-card');
+export const CbpContainer = /*@__PURE__*/createReactComponent<JSX.CbpContainer, HTMLCbpContainerElement>('cbp-container');
+export const CbpUniversalHeader = /*@__PURE__*/createReactComponent<JSX.CbpUniversalHeader, HTMLCbpUniversalHeaderElement>('cbp-universal-header');
