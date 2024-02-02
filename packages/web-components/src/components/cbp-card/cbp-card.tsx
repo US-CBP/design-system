cc23cd3bd661acb055b81aeaf2b364c6d6b1cb73
Doug Gibson <email@dgibson.net>
2024-02-02 16:16:35 -0500

    Updated cards to stretch vertically and fixed link buttons

--- packages/web-components/src/components/cbp-card/cbp-card.tsx
+++ packages/web-components/src/components/cbp-card/cbp-card.tsx
@@ -1,6 +1,11 @@
 import { Component, Host, Element, Prop, h } from '@stencil/core';
 import { setCSSProps } from '../../utils/utils';
 
+/**
+ * @slot - The default slot contains the body of the card.
+ * @slot cbp-card-title - Contains the card title, if present.
+ * @slot cbp-card-actions - Contains the links/buttons for decision cards.
+ */
 @Component({
   tag: 'cbp-card',
   styleUrl: 'cbp-card.scss',
@@ -8,8 +13,15 @@ import { setCSSProps } from '../../utils/utils';
 export class CbpCard {
   @Element() host: HTMLElement;
 
+  /** Optionally specifies a card color (different from the default color) based on predefined design token values. */
   @Prop({ reflect: true }) color: 'info' | 'success' | 'warning' | 'danger';
+  
+  /** Specifies optional variants with difference from the default card. */
   @Prop({ reflect: true }) variant: 'banner' | 'decision';
+
+  /** When present, the card will stretch vertically to fill its parent container; most useful when placed in an equally sized grid or row of cards. */
+  @Prop({ reflect: true }) stretch: boolean = false;
+
   /** Supports adding inline styles as an object */
   @Prop() sx: any = {};
 
