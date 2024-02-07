f300f10a71b67d72f1c0b74394d92e9b22eb1eb4
Doug Gibson <email@dgibson.net>
2024-02-07 09:27:13 -0500

    Update Drawer component to show backdrop, close it when clicked, and close on ESC, added accessibility features.

--- packages/web-components/src/components/cbp-drawer/cbp-drawer.tsx
+++ packages/web-components/src/components/cbp-drawer/cbp-drawer.tsx
@@ -1,5 +1,5 @@
-import { Component, Prop, Element, Event, EventEmitter, Host, h } from '@stencil/core';
-import { setCSSProps } from '../../utils/utils';
+import { Component, Prop, Element, Event, EventEmitter, Method, Watch, Host, h } from '@stencil/core';
+import { setCSSProps, getFocusableElements } from '../../utils/utils';
 
 @Component({
   tag: 'cbp-drawer',
@@ -7,7 +7,8 @@ import { setCSSProps } from '../../utils/utils';
 })
 export class CbpDrawer {
   private drawer: HTMLElement;
-  
+  private focusableElements: any[];
+
   @Element() host: HTMLElement;
 
   @Prop({ reflect: true }) position: 'left' | 'right' = 'left';
@@ -20,7 +21,16 @@ export class CbpDrawer {
   /** Supports adding inline styles as an object */
   @Prop() sx: any = {};
 
-  @Event() drawerToggle!: EventEmitter;
+  /** Custom event fired when the drawer is opened. */
+  @Event() drawerOpen!: EventEmitter;
+  /** Custom event fired when the drawer is closed. */
+  @Event() drawerClose!: EventEmitter;
+
+
+  @Watch('open') 
+  watchOpenHandler(newValue: boolean) {
+    newValue==true ? this.setFocus() : this.closeDrawer();
+  }
 
   /*
   drawerDidUpdate() {
@@ -31,15 +41,42 @@ export class CbpDrawer {
   }
   
 */
-  // Add a class via DOM after the rendering to allow for animation
-  toggleDrawer = () => {
-    this.open=!this.open;
-    this.drawerToggle.emit({
+
+  @Method()
+  async openDrawer() {
+    this.open=true;
+    this.drawerOpen.emit({
       host: this.host,
       open: this.open,
     });
-    //this.setFocusElement();
   }
+
+  @Method()
+  async closeDrawer() {
+    this.open=false;
+    this.drawerClose.emit({
+      host: this.host,
+      open: this.open,
+    });
+  }
+
+  setFocus() {
+    setTimeout( () => {
+      if (!this.focusableElements) {
+        this.focusableElements=getFocusableElements(this.host);  
+      }
+      this.focusableElements[0]?.focus()
+    },100);
+  }
+
+  handleBackdropClick({target}) {
+    !target.closest('.cbp-drawer__content') && this.closeDrawer();
+  }
+
+  handleKeyUp(e) {
+    e.key == 'Escape' && this.closeDrawer();
+  }
+
 
   componentDidLoad() {
     if (typeof this.sx == 'string') {
@@ -61,13 +98,18 @@ export class CbpDrawer {
 
   render() {
     return (
-      <Host>
+      <Host
+        onClick={(e) => this.handleBackdropClick(e)}
+        onKeyUp={(e) => this.handleKeyUp(e)}
+        id={this.uid}
+      >
         <div
-          class="cbp-drawer__content"
+          ref={(el) => this.drawer = el}
           role="dialog"
+          aria-modal="true"
+          class="cbp-drawer__content"
           aria-label={this.accessibilityText}
           tabindex="-1"
-          ref={(el) => this.drawer = el}
         >
           <cbp-button
             class="cbp-drawer__close-button"
@@ -81,7 +123,9 @@ export class CbpDrawer {
           >
             <cbp-icon name="circle-xmark"></cbp-icon>
           </cbp-button>
+
           <slot />
+
         </div>
       </Host>
     );
