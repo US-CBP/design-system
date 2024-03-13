7275c8c5ff813009ca5b13b46d6d3fedafc59cbc
Doug Gibson <email@dgibson.net>
2024-03-13 12:07:30 -0400

    Add icon option to tag story

--- packages/web-components/src/components/cbp-tag/cbp-tag.stories.tsx
+++ packages/web-components/src/components/cbp-tag/cbp-tag.stories.tsx
@@ -9,8 +9,11 @@ export default {
     },
     color: {
       description: 'The color of the tag, from a predefined list of design tokens.',
-      control: { type: 'select' },
+      control: 'select',
       options: ['default', 'danger', 'success', 'warning'],
+    },
+    withIcon: {
+      control: 'boolean',
     },
     sx: {
       description: 'Supports adding inline styles as an object of key-value pairs comprised of CSS properties and values. Values should reference design tokens when possible.',
@@ -22,12 +25,21 @@ export default {
   }
 };
 
-const Template = ({ label, color, sx }) => { 
+const Template = ({ label, color, withIcon, sx }) => { 
+  const Icons = {
+    default: 'circle-info',
+    danger: 'circle-xmark',
+    success: 'check',
+    warning: 'triangle-exclamation'
+  }
+  const Icon = withIcon ? Icons[color] || 'circle-info' : null;
+
   return ` 
     <cbp-tag
       ${(color && color!= 'default') ? `color=${color}` : ''}
       ${sx ? `sx=${JSON.stringify(sx)}` : ''}
     >
+      ${Icon ? `<cbp-icon name="${Icon}" sx='{"margin-right":"var(--cbp-space-2x)"}'></cbp-icon>` : ''}
       ${label}
     </cbp-tag>
   `
