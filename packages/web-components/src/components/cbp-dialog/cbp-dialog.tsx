af30d80b004fe23bfa36d41f2edd219785dec321
Doug Gibson <email@dgibson.net>
2024-06-21 10:16:58 -0400

    Remove commented code and drawer references.

--- packages/web-components/src/components/cbp-dialog/cbp-dialog.tsx
+++ packages/web-components/src/components/cbp-dialog/cbp-dialog.tsx
@@ -4,22 +4,20 @@ import { setCSSProps, getFocusableElements } from '../../utils/utils';
 
 @Component({
   tag: 'cbp-dialog',
-  styleUrl: 'cbp-dialog.scss'
+  styleUrl: 'cbp-dialog.scss',
 })
 export class CbpDialog {
-
   private dialog: HTMLElement;
   private focusableElements: any[];
-
 
   @Element() host: HTMLElement;
 
   /** When set, specifies that the dialog is open */
   @Prop({ reflect: true }) open: boolean;
-  
+
   /** Specifies a unique `ID` for the dialog, used to wire up the controls and accessibility features. */
   @Prop() uid: string;
-  
+
   /** Creates an accessible label for the dialog. */
   @Prop() accessibilityText: string;
 
@@ -29,31 +27,30 @@ export class CbpDialog {
   /** Supports adding inline styles as an object */
   @Prop() sx: any = {};
 
-  /** Custom event fired when the drawer is opened. */
+  /** Custom event fired when the dialog is opened. */
   @Event() dialogOpen!: EventEmitter;
-  /** Custom event fired when the drawer is closed. */
+  /** Custom event fired when the dialog is closed. */
   @Event() dialogClose!: EventEmitter;
 
-
-  @Watch('open') 
+  @Watch('open')
   watchOpenHandler(newValue: boolean) {
-    newValue==true ? this.setFocus() : this.closeDialog();
+    newValue == true ? this.setFocus() : this.closeDialog();
   }
 
-  /** A public method for opening the drawer. */
+  /** A public method for opening the dialog. */
   @Method()
   async openDialog() {
-    this.open=true;
+    this.open = true;
     this.dialogOpen.emit({
       host: this.host,
       open: this.open,
     });
   }
 
-  /** A public method for closing the drawer. */
+  /** A public method for closing the dialog. */
   @Method()
   async closeDialog() {
-    this.open=false;
+    this.open = false;
     this.dialogClose.emit({
       host: this.host,
       open: this.open,
@@ -61,23 +58,19 @@ export class CbpDialog {
   }
 
   setFocus() {
-    setTimeout( () => {
+    setTimeout(() => {
       if (!this.focusableElements) {
-        this.focusableElements=getFocusableElements(this.host);  
+        this.focusableElements = getFocusableElements(this.host);
       }
-      this.focusableElements[0]?.focus()
-      //console.log(this.focusableElements,document.activeElement);
-    },100);
+      this.focusableElements[0]?.focus();
+    }, 100);
   }
 
-
-  handleBackdropClick({target}) {
-    //!target.closest('[role=dialog]') && this.closeDialog();
-    if(!target.closest('[role=dialog]')) {
+  handleBackdropClick({ target }) {
+    if (!target.closest('[role=dialog]')) {
       return;
     }
   }
-
 
   componentDidLoad() {
     if (typeof this.sx == 'string') {
@@ -91,57 +84,41 @@ export class CbpDialog {
   }
 
   componentDidRender() {
+    // Adding/removing a class to support animation
     setTimeout(() => {
-      (this.open) ?
-      this.dialog.classList.add('cbp-dialog--open')
-      : this.dialog.classList.remove('cbp-dialog--open');  
+      this.open ? this.dialog.classList.add('cbp-dialog--open') : this.dialog.classList.remove('cbp-dialog--open');
     }, 10);
-    
   }
 
   render() {
     return (
-      <Host
-        onClick={(e) => this.handleBackdropClick(e)}
-        onKeyDown={ () => {}}
-        id={this.uid}
-      >
-        <div
-          role="dialog"
-          aria-modal="true"
-          aria-label={this.accessibilityText}
-          tabindex="-1"
-          ref={(el) => this.dialog = el}
-        >
+      <Host onClick={e => this.handleBackdropClick(e)} onKeyDown={() => {}} id={this.uid}>
+        <div role="dialog" aria-modal="true" aria-label={this.accessibilityText} tabindex="-1" ref={el => (this.dialog = el)}>
           <div class="cbp-dialog-body">
-            {
-              [
-                {
-                  node: <slot name="cbp-dialog-header" />,
-                  query: 'cbp-dialog-header',
-                },
-                {
-                  node: <slot name="cbp-dialog-body" />,
-                  query: 'cbp-dialog-body',
-                }
-              ].map(({ query, node }) => {
-                const hasSlot = this.host.querySelector(`[slot=${query}]`);
-                if (hasSlot) {
-                  return (node);
-                }
-              })
-            }
+            {[
+              {
+                node: <slot name="cbp-dialog-header" />,
+                query: 'cbp-dialog-header',
+              },
+              {
+                node: <slot name="cbp-dialog-body" />,
+                query: 'cbp-dialog-body',
+              },
+            ].map(({ query, node }) => {
+              const hasSlot = this.host.querySelector(`[slot=${query}]`);
+              if (hasSlot) {
+                return node;
+              }
+            })}
             <slot />
           </div>
-          {
-            this.host.querySelector(`[slot=cbp-dialog-actions]`) && 
-              <div class="cbp-dialog-actions">
-                <slot name="cbp-dialog-actions" />
-              </div>
-          }
+          {this.host.querySelector(`[slot=cbp-dialog-actions]`) && (
+            <div class="cbp-dialog-actions">
+              <slot name="cbp-dialog-actions" />
+            </div>
+          )}
         </div>
       </Host>
     );
   }
-
 }
