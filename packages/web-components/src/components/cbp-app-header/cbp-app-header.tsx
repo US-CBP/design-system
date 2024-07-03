581336a4ab00a2b04543868b779550a7dbc4a98c
Barrett Grubbs <barrettgrubbs@gmail.com>
2024-07-03 13:52:37 -0400

    add context to the component file

--- packages/web-components/src/components/cbp-app-header/cbp-app-header.tsx
+++ packages/web-components/src/components/cbp-app-header/cbp-app-header.tsx
@@ -1,10 +1,14 @@
-import { Component, Host, h } from '@stencil/core';
+import { Component, Host, h, Prop } from '@stencil/core';
 
 @Component({
   tag: 'cbp-app-header',
   styleUrl: 'cbp-app-header.scss'
 })
 export class CbpAppHeader {
+
+  
+  /** Specifies the context of the component as it applies to the visual design and whether it inverts when light/dark mode is toggled. Default behavior is "light-inverts" and does not have to be specified. */
+  @Prop({ reflect: true }) context: "light-inverts" | "light-always" | "dark-inverts" | "dark-always";
 
   render() {
     return (
