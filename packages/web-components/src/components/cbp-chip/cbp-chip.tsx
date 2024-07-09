c6a257cbc17c6497e492daf6974ae19736ee521c
Barrett Grubbs <barrettgrubbs@gmail.com>
2024-07-09 12:36:38 -0400

    Badge, Chip, Tag dark mode

--- packages/web-components/src/components/cbp-chip/cbp-chip.tsx
+++ packages/web-components/src/components/cbp-chip/cbp-chip.tsx
@@ -22,6 +22,9 @@ export class CbpChip {
   /** Specifies the pressed state of the button and `aria-pressed` attribute of the rendered button */
   @Prop() pressed: boolean;
 
+  /** Specifies the context of the component as it applies to the visual design and whether it inverts when light/dark mode is toggled. Default behavior is "light-inverts" and does not have to be specified. */
+  @Prop({ reflect: true }) context: "light-inverts" | "light-always" | "dark-inverts" | "dark-always";
+  
   /** Supports adding inline styles as an object */
   @Prop() sx: any = {};
 
