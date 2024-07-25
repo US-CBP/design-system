0696e7d627bca0224466cf409f675d14f71ed77e
Barrett Grubbs <barrettgrubbs@gmail.com>
2024-07-25 10:55:48 -0400

    Expand dark mode WIP

--- packages/web-components/src/components/cbp-expand/cbp-expand.tsx
+++ packages/web-components/src/components/cbp-expand/cbp-expand.tsx
@@ -31,6 +31,9 @@ export class CbpExpand {
   /** The heading level of the accordion item control. Defaults to h3. */
   @Prop() headingLevel: 'h2' | 'h3' | 'h4' | 'h5' | 'h6' = 'h4';
   
+  /** Specifies the context of the component as it applies to the visual design and whether it inverts when light/dark mode is toggled. Default behavior is "light-inverts" and does not have to be specified. */
+  @Prop({ reflect: true }) context: "light-inverts" | "light-always" | "dark-inverts" | "dark-always";
+  
   /** Supports adding inline styles as an object */
   @Prop() sx: any = {};
 
