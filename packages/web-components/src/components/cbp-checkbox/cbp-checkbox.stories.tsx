6124a3aeda4bd7f88bfa2a7ea2825eee1f443c87
Doug Gibson <email@dgibson.net>
2024-09-11 16:57:28 -0400

    First cut of cbp-checkbox component

---
+++ packages/web-components/src/components/cbp-checkbox/cbp-checkbox.stories.tsx
@@ -0,0 +1,59 @@
+export default {
+  title: 'Components/Checkbox',
+  tags: ['autodocs'],
+  argTypes: {
+    label: {
+      name: 'label (slotted)',
+      description: 'Label text (slotted) for the checkbox.',
+      control: 'text',
+    },
+    name: {
+      description: 'Specifies the `name` attribute of the slotted checkbox.',
+      control: 'text',
+    },
+    value: {
+      description: 'Specifies the `value` attribute of the slotted checkbox.',
+      control: 'text',
+    },
+    checked: {
+      description: 'Specifies the `checked` attribute of the slotted checkbox, which represents its initial checked state only.',
+      control: 'boolean',
+    },
+    disabled: {
+      description: 'Renders the checkbox in a disabled state. A disabled form control does not pass a value on native submit.',
+      control: 'boolean',
+    },
+    context : {
+      control: 'select',
+      options: [ "light-inverts", "light-always", "dark-inverts", "dark-always"]
+    },
+    sx: {
+      description: 'Supports adding inline styles as an object of key-value pairs comprised of CSS properties and values. Values should reference design tokens when possible.',
+      control: 'object',
+    },
+  },
+};
+
+const Template = ({ label, name, value, checked, disabled, context, sx }) => {
+  return ` 
+      <cbp-checkbox
+        ${value ? `value=${value}` : ''}
+        ${disabled ? `disabled=${disabled}` : ''}
+        ${context && context != 'light-inverts' ? `context=${context}` : ''}
+        ${sx ? `sx=${JSON.stringify(sx)}` : ''}
+      >
+        <input 
+          type="checkbox" 
+          name="${name}"
+          value="${value}"
+          ${checked ? `checked=${checked}` : ''}
+        />
+        ${label}
+      </cbp-checkbox>
+    `;
+};
+
+export const Checkbox = Template.bind({});
+Checkbox.args = {
+  label: "Checkbox label"
+}
