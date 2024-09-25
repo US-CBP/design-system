9f02ebeb27d6649897ac9ae3a737a29a2880f34e
Barrett Grubbs <barrettgrubbs@gmail.com>
2024-09-25 11:40:37 -0400

    update logic

--- packages/web-components/src/components/cbp-card/cbp-card.tsx
+++ packages/web-components/src/components/cbp-card/cbp-card.tsx
@@ -70,7 +70,7 @@ export class CbpCard {
                 {(this.variant !== 'banner') && <slot name="cbp-card-title" />}
                 <slot />
             </div>
-            <slot name='cbp-card-actions' />
+            {!this.interactive && <slot name='cbp-card-actions' />}
         </Host>
       )
     }
