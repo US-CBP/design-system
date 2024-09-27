4499344d34e24d41446d6456561041b547a5ad45
Barrett Grubbs <barrettgrubbs@gmail.com>
2024-09-27 14:50:47 -0400

    updates from code review

--- packages/web-components/src/components/cbp-usa-banner/cbp-usa-banner.stories.tsx
+++ packages/web-components/src/components/cbp-usa-banner/cbp-usa-banner.stories.tsx
@@ -1,16 +1,6 @@
 export default {
     title: 'Components/USA Banner',
     tags: ['autodocs'],
-    argTypes: {
-      // context : {
-      //   control: 'select',
-      //   options: [ "light-inverts", "light-always", "dark-inverts", "dark-always"]
-      // },
-      // sx: {
-      //   description: 'Supports adding inline styles as an object of key-value pairs comprised of CSS properties and values. Values should reference design tokens when possible.',
-      //   control: 'object',
-      // },
-    },
   };
   
   const Template = () => {
--- packages/web-components/src/components/cbp-usa-banner/cbp-usa-banner.tsx
+++ packages/web-components/src/components/cbp-usa-banner/cbp-usa-banner.tsx
@@ -11,9 +11,6 @@ export class CbpUsaBanner {
   /** When set, specifies that the banner is open */
   @Prop({ reflect: true }) open: boolean;
 
-  // /** Specifies the context of the component as it applies to the visual design and whether it inverts when light/dark mode is toggled. Default behavior is "light-inverts" and does not have to be specified. */
-  // @Prop({ reflect: true }) context: "light-inverts" | "light-always" | "dark-inverts" | "dark-always";
-  
   // /** A custom event emitted when the banner link control is activated. */
   handleClick() {
     // e.preventDefault();
@@ -28,7 +25,7 @@ export class CbpUsaBanner {
            <div class="cbp-banner__title">
               <img src="../assets/images/us_flag_small.png" height="11" width="16" alt="usa logo" />
               <span>
-                <span>An official website of the United States government.</span>
+                <span>Official website of the United States government.</span>
                 <cbp-button
                   class='cbp-usa-banner-expand'
                   fill='ghost'
@@ -65,7 +62,7 @@ export class CbpUsaBanner {
                 />
                 <div>
                   <strong>Secure .gov websites use HTTPS</strong>
-                  <p>A <strong>lock</strong> (<i class="fas fa-lock"></i>) or <strong>https://</strong> means you&#39;ve safely connected to the .gov website. Share sensitive information only on official, secure websites.</p>
+                  <p>A <strong>lock</strong> (<cbp-icon name='lock' />) or <strong>https://</strong> means you&#39;ve safely connected to the .gov website. Share sensitive information only on official, secure websites.</p>
                 </div>
               </div>
             </div>
