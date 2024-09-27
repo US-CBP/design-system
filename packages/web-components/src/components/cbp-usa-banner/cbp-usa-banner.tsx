c6a88cea65531c4c99b865470167086c3b54e0cf
Barrett Grubbs <barrettgrubbs@gmail.com>
2024-09-27 09:28:43 -0400

    updates from code review

--- packages/web-components/src/components/cbp-usa-banner/cbp-usa-banner.tsx
+++ packages/web-components/src/components/cbp-usa-banner/cbp-usa-banner.tsx
@@ -1,4 +1,4 @@
-import { Component, Prop, Element, Event, EventEmitter, Host, h } from '@stencil/core';
+import { Component, Prop, Element, Host, h } from '@stencil/core';
 
 @Component({
   tag: 'cbp-usa-banner',
@@ -14,71 +14,64 @@ export class CbpUsaBanner {
   // /** Specifies the context of the component as it applies to the visual design and whether it inverts when light/dark mode is toggled. Default behavior is "light-inverts" and does not have to be specified. */
   // @Prop({ reflect: true }) context: "light-inverts" | "light-always" | "dark-inverts" | "dark-always";
   
-  /** Supports adding inline styles as an object */
-  @Prop() sx: any = {};
-  
-  /** A custom event emitted when the banner link control is activated. */
-  @Event() bannerLinkClick: EventEmitter;
-  handleClick(e) {
-    e.preventDefault();
+  // /** A custom event emitted when the banner link control is activated. */
+  handleClick() {
+    // e.preventDefault();
     this.open = !this.open;
-    this.bannerLinkClick.emit({
-      host: this.host,
-      open: this.open,
-    });
+    //set aria-expanded if it isn't comine with button
   }
 
   render() {
     return (
       <Host>
-        <img src="../assets/images/us_flag_small.png" height="11" width="16" alt="usa logo"/>Official website of the United State government. 
-        <cbp-link class='cbp-usa-banner-expand' onClick={() => this.handleClick(event)} href='#'>
-          Here is how you know<cbp-icon name='chevron-right'/>
-        </cbp-link>
-        <div class='cbp-usa-banner-content'>
-          <cbp-icon 
-              class='cbp-banner-content-icon'
-              color="var(--cbp-color-info-light)"
-              name='landmark'
-              size="1.25rem" 
-          />  
-          <div>
-            <cbp-typography
-              tag='h5'
-              sx='{"color": "var(--cbp-usa-banner-color)"}'
-            >
-              Official websites use .gov
-            </cbp-typography>
-            <cbp-typography
-              tag='p'
-              sx='{"margin-bottom":0}'
-            >
-              A <b>.gov</b> website belongs to the official government organization in the United States.
-            </cbp-typography>
-          </div>
-          <cbp-icon 
-              class='cbp-banner-content-icon'
-              color="var(--cbp-color-info-light)"
-              name='lock'
-              size="1.25rem" 
-            />
-          <div>
-            <cbp-typography
-              tag='h5'
-              sx='{"color": "var(--cbp-usa-banner-color)"}'
-            >
-              Secure .gov websites use HTTPS
-            </cbp-typography>
-            <cbp-typography
-              tag='p'
-              sx='{"margin-bottom":0}'
-            >
-              A lock(<cbp-icon name='lock' />) or https:// means you've safetly connected to the .gov website. Share sensitive information only on official, secure websites
-            </cbp-typography>
-          </div>
-          <slot>  
-          </slot>
-        </div>
+         <section aria-label="Official website of the United States government">
+           <header class="cbp-banner">
+      
+           <div class="cbp-banner__title">
+              <img src="../assets/images/us_flag_small.png" height="11" width="16" alt="usa logo" />
+              <span>
+                <span>An official website of the United States government.</span>
+                <cbp-button
+                  class='cbp-usa-banner-expand'
+                  fill='ghost'
+                  accessibility-text='open banner content'
+                  target-prop="open"
+                  onClick={() => this.handleClick()}
+                >
+                  Here is how you know<cbp-icon name='chevron-right'/>
+                </cbp-button>
+              </span>
+            </div>
+            
+            <div class="cbp-usa-banner-content" id="gov-banner">
+              <div>
+                  <cbp-icon 
+                    class='cbp-banner-content-icon'
+                    color="var(--cbp-color-info-light)"
+                    name='landmark'
+                    size="1.25rem" 
+                  />  
+                <div>
+                  <strong>Official websites use .gov</strong>
+                  <p>A <strong>.gov</strong> website belongs to an official government organization in the United States.</p>
+                </div>
+              </div>
+
+              <div>
+                <cbp-icon 
+                  class='cbp-banner-content-icon'
+                  color="var(--cbp-color-info-light)"
+                  name='lock'
+                  size="1.25rem" 
+                />
+                <div>
+                  <strong>Secure .gov websites use HTTPS</strong>
+                  <p>A <strong>lock</strong> (<i class="fas fa-lock"></i>) or <strong>https://</strong> means you&#39;ve safely connected to the .gov website. Share sensitive information only on official, secure websites.</p>
+                </div>
+              </div>
+            </div>
+          </header>
+        </section>
       </Host>
     );
   }
