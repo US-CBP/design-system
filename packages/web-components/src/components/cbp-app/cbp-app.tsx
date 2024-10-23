57593c6597fe8bb8be2bd717fc840d89a4ce5b32
Doug Gibson <email@dgibson.net>
2024-10-23 10:40:31 -0400

    Update app and design tokens docs

--- packages/web-components/src/components/cbp-app/cbp-app.tsx
+++ packages/web-components/src/components/cbp-app/cbp-app.tsx
@@ -1,4 +1,4 @@
-import { Component, Prop, Host, h } from '@stencil/core';
+import { Component, Prop, Host, h, Env } from '@stencil/core';
 
 /*
   An overarching "app" tag can act as a low-barrier way to get core design system elements (CSS, fonts)
@@ -17,13 +17,18 @@ export class CbpApp {
   /** Optionally specifies light/dark mode. This is only needed if the application can change the theme separate from OS settings.  */
   @Prop({reflect: true}) theme: "light" | "dark" | "system" = "system"
 
+  @Prop({reflect: true}) debug: boolean;
+
+  @Prop({reflect: true}) appName: string;
+  @Prop({reflect: true}) appVersion: string;
+
   handleThemeChange(mql) {
     this.theme = mql.matches ? "dark" : "light";
   }
 
   componentDidLoad() {
     const darkMode = window?.matchMedia(`(prefers-color-scheme: dark)`);
-    // Only set up the listener if we're using the system default, otherwise, it's being set manually
+    // Only set up the listener if we're using the system default, otherwise it's being set manually via reactive property
     if (this.theme == "system") {
       darkMode.addEventListener('change', mql => this.handleThemeChange(mql)); // Add an event listener to the media query
       this.handleThemeChange(darkMode); // Run the theme change handler once on load
@@ -31,6 +36,16 @@ export class CbpApp {
   }
 
   render() {
+    // If debug is enabled, write debug info to the console
+    if (this.debug) {
+      let debugInfo = `DEBUGGING INFO:\n===============\n`;
+      if (this.appName) debugInfo += `Application name: ${this.appName}\n`;
+      if (this.appVersion) debugInfo += `Application version: ${this.appVersion}\n`;
+      debugInfo += `CBP Design System version: ${Env.version}\n`;
+      debugInfo += `Built with StencilJS: ${Env.stencil}`;
+      console.log(debugInfo);
+    }
+
     return (
       <Host data-cbp-theme={this.theme}>
         <slot />
