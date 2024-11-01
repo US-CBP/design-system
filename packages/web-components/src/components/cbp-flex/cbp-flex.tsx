741bda85c0ea800fb9b360a297e4bf7f2156e307
Doug Gibson <email@dgibson.net>
2024-11-01 16:04:19 -0400

    Added JSDocs to flex and flex item components, tweaked CSS.

--- packages/web-components/src/components/cbp-flex/cbp-flex.tsx
+++ packages/web-components/src/components/cbp-flex/cbp-flex.tsx
@@ -1,6 +1,9 @@
 import { Component, Prop, Element, Host, h } from '@stencil/core';
 import { setCSSProps } from '../../utils/utils';
 
+/**
+ * @slot - DOM nodes placed in the default slot automatically become flex children. The use of `cbp-flex-item` is only required for granular control of individual flex item properties.
+ */
 @Component({
   tag: 'cbp-flex',
   styleUrl: 'cbp-flex.scss',
@@ -8,15 +11,33 @@ import { setCSSProps } from '../../utils/utils';
 export class CbpFlex {
   @Element() host: HTMLElement;
 
+  /** Specifies the display mode. Defaults to "flex" */
   @Prop({ reflect: true }) display: 'flex' | 'inline-flex' = 'flex';
+  
+  /** Specifies the wrapping behavior of the flex children. Browser default behavior is "nowrap". */
   @Prop() wrap: 'nowrap' | 'wrap' | 'wrap-reverse';
+  
+  /** Specifies how flex items are placed in the flex container by setting the direction of the flex container’s main axis. Defaults to "row" for a horizontal flex context. */
   @Prop() direction: 'row' | 'row-reverse' | 'column' | 'column-reverse' = 'row';
+  
+  /** Specifies the alignment for all of the flex container’s items along the cross-axis. Defaults to "stretch". */
   @Prop() alignItems: 'auto' | 'stretch' | 'flex-start' | 'flex-end' | 'center' | 'baseline' = 'stretch';
+  
+  /** Specifies the alignment of a flex container's items within the flex container (only when there is extra space in the cross-axis). */
   @Prop() alignContent: 'flex-start' | 'flex-end' | 'center' | 'space-between' | 'space-around' | 'stretch' = 'stretch';
+  
+  /** Specifies the alignment of flex items along the main axis (of the current line) of the flex container. */
   @Prop() justifyContent: 'flex-start' | 'flex-end' | 'center' | 'space-between' | 'space-around' | 'space-evenly' = 'flex-start';
+  
+  /** Specifies the gap between items in CSS units (preferably relative units such as rem). Accepts a single value for horizontal and vertical gap or two values representing column gap and row gap, respectively. */
   @Prop() gap: string;
+
+  /** Specifies the size at which the flex children are linearized, specified in CSS units (preferably relative units such as rem). */
   @Prop() breakpoint: string;
+
+  /** @Internal - Not yet implemented */
   @Prop() contentBreakpoint: string;
+  
   /** Supports adding inline styles as an object */
   @Prop() sx: any = {};
 
