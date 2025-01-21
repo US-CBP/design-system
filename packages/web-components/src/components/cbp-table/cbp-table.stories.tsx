e53cc80f4196939e0b7b1e5516514ec7bdb5fff5
Doug Gibson <email@dgibson.net>
2025-01-21 16:10:09 -0500

    Exclude column header column highlighting when sortable control.

--- packages/web-components/src/components/cbp-table/cbp-table.stories.tsx
+++ packages/web-components/src/components/cbp-table/cbp-table.stories.tsx
@@ -79,11 +79,13 @@ function generateTableRows(data, selectable, context) {
 
 const Template = ({ tableData, headers, selectable, striped, hover, columnHover, context, sx }) => {
 
+  /*
   const toolbar=`
     <div slot="cbp-table-toolbar">
       Test toolbar.
     </div>
   `;
+  */
   
   return ` 
       <cbp-table
