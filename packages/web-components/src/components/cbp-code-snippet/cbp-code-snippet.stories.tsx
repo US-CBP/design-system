d126b9cb5b657283fedede44e69372549d2d1f32
Doug Gibson <email@dgibson.net>
2025-03-06 12:20:55 -0500

    Code snippet slight refactor and cleanup

--- packages/web-components/src/components/cbp-code-snippet/cbp-code-snippet.stories.tsx
+++ packages/web-components/src/components/cbp-code-snippet/cbp-code-snippet.stories.tsx
@@ -1,47 +1,52 @@
 export default {
-    title: 'Components/Code Snippet',
-    //tags: ['autodocs'],
-    argTypes: {
-      codeSnippet: {
-        description: 'Code to slot into code snippet',
-        control: 'text',
-      },
-      variant: {
-        description: 'determines the display type of the code snippet',
-        control: 'select',
-        options: [ "inline", "block"]
-      },
-      height: {
-        description: 'sets the max-height on the block variant control',
-        control: 'text',
-        if: { arg: 'variant', eq: 'block' },
-      },
-      context : {
-        control: 'select',
-        options: [ "light-inverts", "light-always", "dark-inverts", "dark-always"]
-      },
-      sx: {
-        description: 'Supports adding inline styles as an object of key-value pairs comprised of CSS properties and values. Values should reference design tokens when possible.',
-        control: 'object',
-      },
+  title: 'Components/Code Snippet',
+  tags: ['new'],
+  argTypes: {
+    codeSnippet: {
+      description: 'Code to slot into code snippet',
+      control: 'text',
+      name: 'Code (slotted)'
     },
-    args: {     
-      /** code snippet passed to the <code> block so any indent/spacing will be in final render*/
-      codeSnippet: `<a href='#'> test code snippet </a>`,
+    variant: {
+      description: 'Determines the display type of the code snippet (component default is "inline")',
+      control: 'select',
+      options: ['inline', 'block'],
     },
-  };
-  
-  const Template = ({codeSnippet, variant, height, context, sx}) => {
-    return ` 
-    <cbp-code-snippet
-      ${variant ? `variant= ${variant}` : ``}
-      ${height ? `height= ${height}` : ``}
-      ${context && context != 'light-inverts' ? `context=${context}` : ''}
-      ${sx ? `sx=${JSON.stringify(sx)}` : ''}
-    >
-      ${codeSnippet}
-    </cbp-code-snippet>
-      `;
-  };
-  export const CodeSnippet = Template.bind({});
-  
+    height: {
+      description: 'Sets the height of the block variant (not expanded)',
+      control: 'text',
+      if: { arg: 'variant', eq: 'block' },
+    },
+    /*
+    expanded: {
+      control: 'boolean',
+      if: { arg: 'height', truthy: true },
+    },
+    */
+    context: {
+      control: 'select',
+      options: ['light-inverts', 'light-always', 'dark-inverts', 'dark-always'],
+    },
+    sx: {
+      description: 'Supports adding inline styles as an object of key-value pairs comprised of CSS properties and values. Values should reference design tokens when possible.',
+      control: 'object',
+    },
+  },
+  args: {
+    codeSnippet: `<a href='#'>test code snippet</a>`,
+  },
+};
+
+const Template = ({ codeSnippet, variant, height, context, sx }) => {
+  return ` 
+      <cbp-code-snippet
+        ${variant ? `variant= ${variant}` : ''}
+        ${height ? `height= ${height}` : ''}
+        ${context && context != 'light-inverts' ? `context=${context}` : ''}
+        ${sx ? `sx=${JSON.stringify(sx)}` : ''}
+      >
+        ${codeSnippet}
+      </cbp-code-snippet>
+    `;
+};
+export const CodeSnippet = Template.bind({});
