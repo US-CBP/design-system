07c7d11f2499d379a08672655ae6a6d69abac2f7
Barrett Grubbs <barrettgrubbs@gmail.com>
2025-04-10 14:07:40 -0400

    updates from code review

--- packages/web-components/src/components/cbp-subnav/cbp-subnav.stories.tsx
+++ packages/web-components/src/components/cbp-subnav/cbp-subnav.stories.tsx
@@ -6,15 +6,14 @@ export default {
             description: 'Sets accessibilitytext prop for the subnav component',
             control: 'text',
         },
-        indentation: {
+        flat: {
             description: '',  
             control: 'boolean'
         },
-        //TODO: no dark mode in spec for this component, leaving commented out incase we want to add dark mode later
-        // context : {
-        //     control: 'select',
-        //     options: [ "light-inverts", "light-always", "dark-inverts", "dark-always"]
-        // },
+        context : {
+            control: 'select',
+            options: [ "light-inverts", "light-always", "dark-inverts", "dark-always"]
+        },
         sx: {
             description: 'Supports adding inline styles as an object of key-value pairs comprised of CSS properties and values. Values should reference design tokens when possible.',
             control: 'object',
@@ -22,20 +21,21 @@ export default {
     },
   };
 
-function generateContent(items){
+function generateContent(items, context){
     const html = items.map(({ icon, label, href, open, children, current }) => {
-        return `<cbp-subnav-item label="${label}" href=${href} ${current? `current=${current}` : ``} ${open? `open=${open}` : ``}>${icon ? `<span slot="cbp-subnav-item-label">${icon} ${label}</span>` : ``} ${children? generateContent(children) : ``}</cbp-subnav-item>`;
+        return `<cbp-subnav-item label="${label}" href=${href} ${current? `current=${current}` : ``} ${open? `open=${open}` : ``} ${context && context != 'light-inverts' ? `context=${context}` : ''}>${icon ? `<span slot="cbp-subnav-item-label">${icon} ${label}</span>` : ``} ${children? generateContent(children, context) : ``}</cbp-subnav-item>`;
     });
         return html.join('');
 }
     
-const SubnavTemplate = ({items, accessibilitytext, indentation}) => {
+const SubnavTemplate = ({items, accessibilitytext, flat, context}) => {
     return ` 
         <cbp-subnav
             ${accessibilitytext ? `accessibilitytext="${accessibilitytext}"`: ``}
-            indentation= ${indentation}
+            ${flat ? 'flat' : ''}
+            ${context && context != 'light-inverts' ? `context=${context}` : ''}
             >
-            ${generateContent(items)}
+            ${generateContent(items, context)}
         </cbp-subnav>
     `;
 };
--- packages/web-components/src/components/cbp-subnav/cbp-subnav.tsx
+++ packages/web-components/src/components/cbp-subnav/cbp-subnav.tsx
@@ -12,7 +12,7 @@ export class CbpSubNav {
   @Prop() accessibilitytext: string = 'Sub-Navigation';
 
   /** used to toggle child indenation */
-  @Prop({ reflect: true }) indentation: boolean = true;
+  @Prop({ reflect: true }) flat: boolean;
 
   /** Specifies the context of the component as it applies to the visual design and whether it inverts when light/dark mode is toggled. Default behavior is "light-inverts" and does not have to be specified. */
   @Prop({ reflect: true }) context: "light-inverts" | "light-always" | "dark-inverts" | "dark-always";
