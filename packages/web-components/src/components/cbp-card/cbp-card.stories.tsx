da252a22383ed76178a73e9ca29b2a2a1667abf6
Barrett Grubbs <barrettgrubbs@gmail.com>
2025-05-15 13:22:56 -0400

    update states for interactive multi card

--- packages/web-components/src/components/cbp-card/cbp-card.stories.tsx
+++ packages/web-components/src/components/cbp-card/cbp-card.stories.tsx
@@ -187,6 +187,71 @@ const InteractiveTemplate = ({ title, color, disabled, bodyText, withIcon, inter
           </cbp-typography>`}
       <p>${bodyText}</p>  
     </cbp-card>
+
+    ${interactive === 'multi' ? `
+    <cbp-card
+      ${variant !=='general' ? `variant="${variant}"` : ''}
+      ${interactive ? `interactive=${interactive}` : ''}
+      ${href ? `href="${href}"` : ''}
+      ${disabled ? `disabled=${disabled}` : ''}
+      ${color ? `color=${color}` : ''}
+      ${context && context != 'light-inverts' ? `context=${context}` : ''}
+      ${sx ? `sx='${JSON.stringify(sx)}'` : ''}
+    >
+      
+      ${variant=== 'flag' ? `<div slot="cbp-card-flag"><img src="https://api.dicebear.com/9.x/personas/svg" /></div>` : ''}
+      ${interactive === 'multi' ? `
+        <cbp-checkbox value="1" slot="cbp-card-title">
+          <cbp-typography tag="h4" sx='{"color":"var(--cbp-card-color-title)"}'>
+              ${withIcon ? `<cbp-icon name="triangle-exclamation" size="1.25rem"></cbp-icon>` : ''}
+              ${title}
+          </cbp-typography>
+          <input
+            type="checkbox"
+            name="multiCard"
+            value="1"
+            style="margin-left: auto"
+          />
+        </cbp-checkbox>
+      ` 
+      :   `<cbp-typography tag="h4" slot="cbp-card-title">
+            ${withIcon ? `<cbp-icon name="triangle-exclamation" size="1.25rem"></cbp-icon>` : ''}
+            ${title}
+          </cbp-typography>`}
+      <p>${bodyText}</p>  
+    </cbp-card>
+    <cbp-card
+      ${variant !=='general' ? `variant="${variant}"` : ''}
+      ${interactive ? `interactive=${interactive}` : ''}
+      ${href ? `href="${href}"` : ''}
+      ${disabled ? `disabled=${disabled}` : ''}
+      ${color ? `color=${color}` : ''}
+      ${context && context != 'light-inverts' ? `context=${context}` : ''}
+      ${sx ? `sx='${JSON.stringify(sx)}'` : ''}
+    >
+      
+      ${variant=== 'flag' ? `<div slot="cbp-card-flag"><img src="https://api.dicebear.com/9.x/personas/svg" /></div>` : ''}
+      ${interactive === 'multi' ? `
+        <cbp-checkbox value="1" slot="cbp-card-title">
+          <cbp-typography tag="h4" sx='{"color":"var(--cbp-card-color-title)"}'>
+              ${withIcon ? `<cbp-icon name="triangle-exclamation" size="1.25rem"></cbp-icon>` : ''}
+              ${title}
+          </cbp-typography>
+          <input
+            type="checkbox"
+            name="multiCard"
+            value="1"
+            style="margin-left: auto"
+          />
+        </cbp-checkbox>
+      ` 
+      :   `<cbp-typography tag="h4" slot="cbp-card-title">
+            ${withIcon ? `<cbp-icon name="triangle-exclamation" size="1.25rem"></cbp-icon>` : ''}
+            ${title}
+          </cbp-typography>`}
+      <p>${bodyText}</p>  
+    </cbp-card>
+    ` : null}
   `;
 };
 
