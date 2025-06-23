481ea81fc64d63a07eec9b7785fa6dba8e96584e
Doug Gibson <email@dgibson.net>
2025-06-23 16:39:40 -0400

    Updated breadcrumbs component with responsive behavior.

--- packages/web-components/src/components/cbp-breadcrumb/cbp-breadcrumb.stories.tsx
+++ packages/web-components/src/components/cbp-breadcrumb/cbp-breadcrumb.stories.tsx
@@ -1,64 +1,88 @@
 export default {
-    title: 'Components/Breadcrumb',
-    tags: ['beta'],
-    argTypes: {
-      context : {
-        control: 'select',
-        options: [ "light-inverts", "light-always", "dark-inverts", "dark-always"]
-      },
-      sx: {
-        description: 'Supports adding inline styles as an object of key-value pairs comprised of CSS properties and values. Values should reference design tokens when possible.',
-        control: 'object',
-      },
+  title: 'Components/Breadcrumb',
+  tags: ['beta'],
+  argTypes: {
+    divider: {
+      control: 'text',
     },
-      args: {
-      },
-    };
-  
-    function generateBreadcrumbs(breadcrumbs, context){
-      const html =  breadcrumbs.map(({text, href}) => {
-          return ` / <cbp-link href=${href}  context=${context}> ${text}</cbp-link> `
-        }
-      );
-      return html.join('');
-    }
+    context: {
+      control: 'select',
+      options: ["light-inverts", "light-always", "dark-inverts", "dark-always"]
+    },
+    sx: {
+      description: 'Supports adding inline styles as an object of key-value pairs comprised of CSS properties and values. Values should reference design tokens when possible.',
+      control: 'object',
+    },
+  },
+  args: {
+  },
+};
 
-    const Template = ({ breadcrumbs, home, context, sx}) => {
-        return ` 
-            <cbp-breadcrumb
-              ${context && context != 'light-inverts' ? `context=${context}` : ''}
-              ${sx ? `sx=${JSON.stringify(sx)}` : ''}
-            >
-              <cbp-button
-                tag="a"
-                fill="ghost"
-                color="primary"
-                variant="square"
-                context=${context}
-                href=${home}
-                accessibility-text="Home"
-              >
-                <cbp-icon
-                  name="home"
-                >
-                </cbp-icon>
-              </cbp-button>
-              ${generateBreadcrumbs(breadcrumbs, context)}
-            </cbp-breadcrumb>
-          `;
-      };
-    
-      export const Breadcrumb = Template.bind({});
-        Breadcrumb.args = {
-            breadcrumbs: [
-              {
-                text: 'Test',
-                href: '#'
-              },
-              {
-                text: 'Test 2',
-                href: '#'
-              },
-            ], 
-            home: '#'
-        };
+function generateBreadcrumbs(breadcrumbs, context) {
+  const html = breadcrumbs.map(({ text, href }) => {
+    return `<cbp-link href=${generateUnvisitedLink(href)} ${context && context != 'light-inverts' ? `context=${context}` : ''}>${text}</cbp-link>`
+  }
+  );
+  return html.join('');
+}
+
+// Replace placeholder href="#" with unique URLs that do not show as "visited" (a common problem when using # or self-referencing URLs);
+// Should also use preventDefault() to avoid navigation to a broken link.
+function generateUnvisitedLink(href) {
+  if(href == '#') return '?' + (Math.random() + 1).toString(26).slice(2, 7);
+  else return href;
+}
+
+const Template = ({ breadcrumbs, home, divider, context, sx }) => {
+  // preventDefault on all links in the breadcrumbs
+  setTimeout(() => {
+    let anchors = document.querySelectorAll('cbp-breadcrumb a');
+    anchors.forEach(anchor => {
+      anchor.addEventListener('click', function (e) { e.preventDefault(); })
+    });
+  }, 1000);
+
+  return ` 
+    <cbp-breadcrumb
+      ${divider ? `divider="${divider}"` : ''}
+      ${context && context != 'light-inverts' ? `context="${context}"` : ''}
+      ${sx ? `sx=${JSON.stringify(sx)}` : ''}
+    >
+      <cbp-button
+        tag="a"
+        fill="ghost"
+        color="primary"
+        variant="square"
+        ${context && context != 'light-inverts' ? `context="${context}"` : ''}
+        href="${home}"
+        accessibility-text="Home"
+      >
+        <cbp-icon name="home"></cbp-icon>
+      </cbp-button>
+      ${generateBreadcrumbs(breadcrumbs, context)}
+    </cbp-breadcrumb>
+  `;
+};
+
+export const Breadcrumb = Template.bind({});
+Breadcrumb.args = {
+  breadcrumbs: [
+    {
+      text: 'Page Title Level A',
+      href: '#'
+    },
+    {
+      text: 'Page Title Level B',
+      href: '#'
+    },
+    {
+      text: 'Page Title Level C',
+      href: '#'
+    },
+    {
+      text: 'Page Title Level D',
+      href: '#'
+    },
+  ],
+  home: '#'
+};
