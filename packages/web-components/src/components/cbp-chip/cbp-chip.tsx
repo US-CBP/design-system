fdf57854a4771f5478249ddc3f7772fc2dc0276a
Doug Gibson <email@dgibson.net>
2025-07-09 16:36:04 -0400

    Chip - accepts a boolean, but converts to a string - fixed by defaulting to false, which improves accessibility and feedback on state changes/user interaction.

--- packages/web-components/src/components/cbp-chip/cbp-chip.tsx
+++ packages/web-components/src/components/cbp-chip/cbp-chip.tsx
@@ -27,7 +27,7 @@ export class CbpChip {
   @Prop() value: string;
   
   /** Specifies the pressed state of the button and `aria-pressed` attribute of the rendered button */
-  @Prop() pressed: boolean;
+  @Prop() pressed: boolean = false;
 
   /** Specifies the context of the component as it applies to the visual design and whether it inverts when light/dark mode is toggled. Default behavior is "light-inverts" and does not have to be specified. */
   @Prop({ reflect: true }) context: "light-inverts" | "light-always" | "dark-inverts" | "dark-always";
