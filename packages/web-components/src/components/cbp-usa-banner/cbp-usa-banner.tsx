44c8d035c4bb39f7bbffc3b8ab6b076140cf14f3
Doug Gibson <email@dgibson.net>
2025-07-22 16:47:32 -0400

    Added accessible text to the lock icon for better reading.

--- packages/web-components/src/components/cbp-usa-banner/cbp-usa-banner.tsx
+++ packages/web-components/src/components/cbp-usa-banner/cbp-usa-banner.tsx
@@ -69,7 +69,7 @@ export class CbpUsaBanner {
               />
               <div>
                 <strong>Secure .gov websites use HTTPS</strong>
-                <p>A <strong>lock</strong> (<cbp-icon name="lock" size="var(--cbp-space-3x)" />) or <strong>https://</strong> means you&#39;ve safely connected to the .gov website. Share sensitive information only on official, secure websites.</p>
+                <p>A <strong>lock</strong> (<cbp-icon name="lock" size="var(--cbp-space-3x)" accessibilityText="lock icon"/>) or <strong>https://</strong> means you&#39;ve safely connected to the .gov website. Share sensitive information only on official, secure websites.</p>
               </div>
             </div>
           </div>
