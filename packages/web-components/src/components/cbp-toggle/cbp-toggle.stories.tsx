491fa438029ebc46b3ccae0e714b7c44a38244ea
Barrett Grubbs <barrettgrubbs@gmail.com>
2025-08-18 12:58:23 -0400

    addressing checked disabled bug and var name for --cbp-toggle-status-text-width

--- packages/web-components/src/components/cbp-toggle/cbp-toggle.stories.tsx
+++ packages/web-components/src/components/cbp-toggle/cbp-toggle.stories.tsx
@@ -54,7 +54,8 @@ const Template = ({label, checked, name, value, hideStatus, statusTextOn, status
 export const Toggle = Template.bind({});
   
 Toggle.args = {
-    label: 'Toggle Label'
+    label: 'Toggle Label',
+    checked: false
 }
 Toggle.argTypes = {
     label: {
