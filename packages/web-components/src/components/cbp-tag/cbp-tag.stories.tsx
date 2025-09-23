9dcb5bc59954e3a921e9126a2cbd2a8c6f50d95b
Doug Gibson <email@dgibson.net>
2025-09-23 11:40:31 -0400

    Attempt CSF3 story on tag component (commented out)

--- packages/web-components/src/components/cbp-tag/cbp-tag.stories.tsx
+++ packages/web-components/src/components/cbp-tag/cbp-tag.stories.tsx
@@ -52,3 +52,82 @@ const Template = ({ label, color, withIcon, context, sx }) => {
 
 export const Tag = Template.bind({});
 Tag.args = {};
+
+
+
+
+
+
+/*
+  Attempt at CSF3:
+    * Has issues with the decorator wrapping the story in `cbp-app` tags.
+      * Using the newer documented method doesn't wrap the story in `cbp-app`.
+      * Using the old (current) method renders the story contents as "[object] [Object]".
+    * Autopopulates many component props in the control panel automatically, but shows them as "object" - need to define them as argTypes to work properly anyway.  
+    * Displays additional unwanted properties in the controls panel.
+*/
+
+/*
+import type { Meta, StoryObj } from '@storybook/web-components-vite';
+import { html } from 'lit';
+
+const meta: Meta = {
+  title: 'Components/Tag',
+  tags: ['beta'],
+  component: 'cbp-tag',
+  argTypes: {
+    label: {
+      name: 'label (slotted)',
+      control: 'text',
+    },
+    color: {
+      control: 'select',
+      options: ['default', 'danger', 'success', 'warning'],
+    },
+    withIcon: {
+      control: 'boolean',
+    },
+    context : {
+      control: 'select',
+      options: [ "light-inverts", "light-always", "dark-inverts", "dark-always"]
+    },
+    sx: {
+      control: 'object',
+    },
+  },
+};
+
+export default meta;
+type Story = StoryObj;
+
+//  By default, stories will render the component defined in the meta (default export), with the args passed to it. 
+//  If you need to render something else, you can provide a function to the render property that returns the desired output.
+export const Basic: Story = {};
+ 
+export const WithProp: Story = {
+  render: () => html`
+    <div>
+      Test
+    </div>
+  `,
+};
+
+
+export const Tag: Story = {
+  args: {
+    label: 'Tag',
+    color: undefined,
+    context: undefined,
+    sx: undefined
+  },
+  render: (args) => html`
+    <cbp-tag
+      ${args.color ? `color="${args.color}"` : ''}
+      ${args.context ? `context="${args.context}"` : ''}
+      ${args.sx ? `sx='${args.sx}'` : ''}
+    >
+      ${args.label}
+    </cbp-tag>
+  `,
+};
+*/
--- packages/web-components/src/components/cbp-tag/cbp-tag.tsx
+++ packages/web-components/src/components/cbp-tag/cbp-tag.tsx
@@ -2,7 +2,7 @@ import { Component, Prop, Element, Host, h } from '@stencil/core';
 import { setCSSProps } from '../../utils/utils';
 
 /**
- * The Tag component is a visual treatment for text, typically representing labels, keywords, or search terms.
+ * The Tag component is a non-interactive visual treatment for text, typically representing labels, keywords, or search terms.
  * 
  * @slot - The tag label or content is placed in the default slot.
  */
@@ -14,6 +14,7 @@ export class CbpTag {
 
   @Element() host: HTMLElement;
 
+  /** Specifies a tag color variant. Default does not need to be specified. */
   @Prop({ reflect: true }) color: 'default' | 'danger' | 'success' | 'warning';
 
   /** Specifies the context of the component as it applies to the visual design and whether it inverts when light/dark mode is toggled. Default behavior is "light-inverts" and does not have to be specified. */
@@ -21,6 +22,7 @@ export class CbpTag {
   
   /** Supports adding inline styles as an object */
   @Prop() sx: any = {};
+
 
   componentWillLoad() {
     if (typeof this.sx == 'string') {
@@ -38,5 +40,4 @@ export class CbpTag {
       </Host>
     );
   }
-
 }
