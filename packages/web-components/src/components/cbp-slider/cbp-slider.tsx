0be9b446161c2b0108b7680cd95c5abac0d63c83
Doug Gibson <email@dgibson.net>
2026-02-09 16:33:25 -0500

    Update slider docs

--- packages/web-components/src/components/cbp-slider/cbp-slider.tsx
+++ packages/web-components/src/components/cbp-slider/cbp-slider.tsx
@@ -30,8 +30,11 @@ export class CbpSlider {
   @Prop() fieldId: string = createNamespaceKey('cbp-slider');
 
   /** 
-   *  Specifies the value of the slider and numeric entry field.
-   *  This prop should be set on this component rather than (or in addition to) the slotted `input type="range"`. 
+   * Specifies the value of the slider and numeric entry field. It is highly recommended to set a contextually sensible default because
+   * the native `input type="range" cannot have an undefined or empty string as a value. If no value is specified, the default value will 
+   * be reported (and submitted) as halfway between the specified minimum and maximum; unless the maximum is actually less than the minimum, 
+   * in which case the default is set to the value of the minimum attribute.
+   * This prop should be set on this component rather than (or in addition to) the slotted `input type="range"`. 
    */
   @Prop({ mutable: true, reflect: true }) value: number | number[] | string | string[];
 
