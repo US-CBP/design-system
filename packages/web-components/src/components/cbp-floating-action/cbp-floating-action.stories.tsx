a13b90c7db8f7dde0e0caf3bbe2a5a5b498b52fb
bagrub <barrettgrubbs@gmail.com>
2026-03-19 16:32:28 -0400

    WIP update alignment prop to top, left, bottom, right

--- packages/web-components/src/components/cbp-floating-action/cbp-floating-action.stories.tsx
+++ packages/web-components/src/components/cbp-floating-action/cbp-floating-action.stories.tsx
@@ -2,26 +2,47 @@ export default {
   title: 'Components/Floating Action',
   tags: ['beta'],
   argTypes: {
-    alignment : {
-      description: 'Determines the alignment of the Floating Action Container',
-      control: 'select',
-      options: ['bottom-left', 'bottom-right', 'top-left', 'top-right']
+    top:{
+        description: 'Set the top CSS property for the floating action',
+        control: 'text' //TODO: is this correct control?
+    },
+    right:{
+        description: 'Set the right CSS property for the floating action',
+        control: 'text' //TODO: is this correct control?
+    },
+    bottom:{
+        description: 'Set the bottom CSS property for the floating action',
+        control: 'text' //TODO: is this correct control?
+    },
+    left:{
+        description: 'Set the left CSS property for the floating action',
+        control: 'text' //TODO: is this correct control?
     },
     additionalButton:{
         description: 'Render an additional button in the Floating Action',
         control: 'boolean'
-    }
+    },
+    sx: {
+      description: 'Supports adding inline styles as an object of key-value pairs comprised of CSS properties and values. Values should reference design tokens when possible.',
+      control: 'object',
+    },
   },
   args: {
-    alignment: 'bottom-right',
+    // top:'',
+    // left:'',
+    bottom: '1rem',
+    right:'1rem',
     additionalButton: false
   },
 };
 
-const Template = ({ alignment, additionalButton }) => { 
+const Template = ({top, right, bottom, left, additionalButton }) => { //TODO: issue with spacing on top/right/bottom/left making render weird when using control
   return `
     <cbp-floating-action
-        alignment= ${alignment}
+        top=${top}
+        right=${right}
+        bottom=${bottom}
+        left=${left}
     >
       <cbp-button
         color=primary 
--- packages/web-components/src/components/cbp-floating-action/cbp-floating-action.tsx
+++ packages/web-components/src/components/cbp-floating-action/cbp-floating-action.tsx
@@ -1,4 +1,5 @@
-import { Component, Host, h, Prop} from '@stencil/core';
+import { Component, Element, Host, h, Prop} from '@stencil/core';
+import { setCSSProps } from '../../utils/utils';
 
 @Component({
   tag: 'cbp-floating-action',
@@ -6,8 +7,39 @@ import { Component, Host, h, Prop} from '@stencil/core';
 })
 export class CbpFloatingAction {
 
+
+  @Element() private host: HTMLElement;
+
   /**determines the alignment of the floating action component. defaults to bottom-right*/
-  @Prop() alignment: 'bottom-left' | 'bottom-right' | 'top-left' | 'top-right' = 'bottom-right';
+  // @Prop() alignment: 'bottom-left' | 'bottom-right' | 'top-left' | 'top-right' = 'bottom-right';
+
+  /**sets the top css property of the floating action component */
+  @Prop() top: any //TODO: i think there is a better type for this? like CSSProperty or something
+
+  /**sets the top css property of the floating action component */
+  @Prop() right: any //TODO: i think there is a better type for this? like CSSProperty or something
+
+  /**sets the top css property of the floating action component */
+  @Prop() bottom: any //TODO: i think there is a better type for this? like CSSProperty or something
+
+  /**sets the top css property of the floating action component */
+  @Prop() left: any //TODO: i think there is a better type for this? like CSSProperty or something
+
+  /** Supports adding inline styles (to the host) as an object. This property is not reactive. */
+  @Prop() sx: any = {};
+
+  componentWillLoad() {
+      if (typeof this.sx == 'string') {
+        this.sx = JSON.parse(this.sx) || {};
+      }
+      setCSSProps(this.host, {
+        top: this.top,
+        right: this.right,
+        bottom: this.bottom,
+        left: this.left,
+        ...this.sx,
+      });
+    }
 
   render() {
     return (
