cd01ecd87178e9688ee40dca9d1f1289068ff5fb
bagrub <barrettgrubbs@gmail.com>
2026-04-29 10:07:51 -0400

    updates to doc and scroll based on sync discussion

--- packages/web-components/src/components/cbp-toast-container/cbp-toast-container.tsx
+++ packages/web-components/src/components/cbp-toast-container/cbp-toast-container.tsx
@@ -1,5 +1,10 @@
 import { Component, Host, h, Prop } from '@stencil/core';
 
+/**
+ * The Toast container is to position a collection of toast components in app
+ * @slot - The default slot is to be populated with cbp-toasts
+ */
+
 @Component({
   tag: 'cbp-toast-container',
   styleUrl: 'cbp-toast-container.scss'
