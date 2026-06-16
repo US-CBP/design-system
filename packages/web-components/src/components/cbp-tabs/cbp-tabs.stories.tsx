abefea16a3850c69ac74462f5c1af3049305fbd3
bagrub <barrettgrubbs@gmail.com>
2026-06-16 14:19:52 -0400

    Update toast container width

--- packages/web-components/src/components/cbp-tabs/cbp-tabs.stories.tsx
+++ packages/web-components/src/components/cbp-tabs/cbp-tabs.stories.tsx
@@ -73,7 +73,6 @@ const Template = ({ tabs, orientation, gridTemplateColumns, accessibilityText, w
     <cbp-grid 
       grid-template-columns="${gridTemplateColumns}" 
       gap="1rem"
-      sx ='{"height": "30rem"}'
     >
 
         <cbp-tabs
