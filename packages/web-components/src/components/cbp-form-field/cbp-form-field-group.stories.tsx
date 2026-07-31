91c9e424c402f4182af47da8c22ced304eb531e3
Doug Gibson <email@dgibson.net>
2026-07-31 15:19:20 -0400

    Add some form field group stproes as well as form field groups as dividers

--- packages/web-components/src/components/cbp-form-field/cbp-form-field-group.stories.tsx
+++ packages/web-components/src/components/cbp-form-field/cbp-form-field-group.stories.tsx
@@ -41,6 +41,115 @@ export default {
 };
 
 
+// Countries: 
+const Countries=[{"label":"Afghanistan"},{"label":"Albania"},{"label":"Algeria"},{"label":"Andaman Islands"},{"label":"Andorra"},{"label":"Angola"},{"label":"Anguilla"},{"label":"Annobon Island"},{"label":"Antigua"},{"label":"Antigua and Barbuda"},{"label":"Argentina"},{"label":"Armenia"},{"label":"Aruba"},{"label":"Ascension Island"},{"label":"Australia"},{"label":"Austria"},{"label":"Azerbaijan"},{"label":"Azores"},{"label":"Bahamas"},{"label":"Bahrain"},{"label":"Balearic Islands"},{"label":"Bangladesh"},{"label":"Barbados"},{"label":"Barbuda"},{"label":"Basse Terre"},{"label":"Belarus"},{"label":"Belau"},{"label":"Belgium"},{"label":"Belize"},{"label":"Benin"},{"label":"Bermuda"},{"label":"Bhutan"},{"label":"Bolivia"},{"label":"Bonaire"},{"label":"Bosnia and Herzegovina"},{"label":"Bosnia-Herzegovina"},{"label":"Botswana"},{"label":"Brazil"},{"label":"British Virgin Islands"},{"label":"Brunei"},{"label":"Bulgaria"},{"label":"Burkina Faso"},{"label":"Burma"},{"label":"Burundi"},{"label":"Byelarus"},{"label":"Cabinda"},{"label":"Caicos Islands"},{"label":"Cambodia"},{"label":"Cameroon"},{"label":"Canada"},{"label":"Canary Islands"},{"label":"Canton Islands"},{"label":"Cape Verde"},{"label":"Carriacou"},{"label":"Castelrosse Islands"},{"label":"Cayman Islands"},{"label":"Central African Rep"},{"label":"Chad"},{"label":"Channel Islands"},{"label":"Chile"},{"label":"China"},{"label":"Christmas Island"},{"label":"Cocos Islands"},{"label":"Colombia"},{"label":"Comoros"},{"label":"Congo"},{"label":"Cook Islands"},{"label":"Corsica"},{"label":"Costa Rica"},{"label":"Crete"},{"label":"Croatia"},{"label":"Cuba"},{"label":"Curacao"},{"label":"Cyprus"},{"label":"Czech Republic"},{"label":"Dem Rep of Congo"},{"label":"Denmark"},{"label":"Diego Garcia"},{"label":"Djibouti"},{"label":"Dodecanese Islands"},{"label":"Dominica"},{"label":"Dominican Republic"},{"label":"East Timor"},{"label":"Easter Island"},{"label":"Ecuador"},{"label":"Egypt"},{"label":"Eire"},{"label":"El Salvador"},{"label":"England"},{"label":"Equatorial Guinea"},{"label":"Eritrea"},{"label":"Estonia"},{"label":"Ethiopia"},{"label":"Falkland Islands"},{"label":"Faroe Islands"},{"label":"Fernando de Noronha"},{"label":"Fiji"},{"label":"Finland"},{"label":"France"},{"label":"French Guiana"},{"label":"French Polynesia"},{"label":"French West Indies"},{"label":"Gabon"},{"label":"Gambia"},{"label":"Georgia"},{"label":"Germany"},{"label":"Ghana"},{"label":"Gibraltar"},{"label":"Grand Cayman Island"},{"label":"Grand Terre"},{"label":"Grand Turk"},{"label":"Great Britain"},{"label":"Greece"},{"label":"Greenland"},{"label":"Grenada"},{"label":"Grenadine Islands"},{"label":"Guadeloupe"},{"label":"Guatemala"},{"label":"Guinea"},{"label":"Guinea-Bissau"},{"label":"Guyana"},{"label":"Haiti"},{"label":"Honduras"},{"label":"Hong Kong"},{"label":"Hungary"},{"label":"Iceland"},{"label":"India"},{"label":"Indonesia"},{"label":"Iran"},{"label":"Iraq"},{"label":"Ireland"},{"label":"Ireland, Republic of"},{"label":"Isle of Man"},{"label":"Israel"},{"label":"Italy"},{"label":"Jamaica"},{"label":"Jan Mayen Island"},{"label":"Japan"},{"label":"Jerusalem"},{"label":"Jordan"},{"label":"Kampuchea"},{"label":"Kazakhstan"},{"label":"Kenya"},{"label":"Kiribati"},{"label":"Kuwait"},{"label":"Kyrgyzstan"},{"label":"Laos"},{"label":"Latvia"},{"label":"Lebanon"},{"label":"Lesotho"},{"label":"Liberia"},{"label":"Libya"},{"label":"Liechtenstein"},{"label":"Lithuania"},{"label":"Little Cayman Island"},{"label":"Luxembourg"},{"label":"Macao"},{"label":"Macedonia"},{"label":"Madagascar"},{"label":"Madeira Islands"},{"label":"Malawi"},{"label":"Malaysia"},{"label":"Maldives"},{"label":"Mali"},{"label":"Malta"},{"label":"Marshall Islands"},{"label":"Martinique"},{"label":"Mauritania"},{"label":"Mauritius"},{"label":"Mexico"},{"label":"Micronesia"},{"label":"Midway Island"},{"label":"Miquelon Island"},{"label":"Moldova"},{"label":"Monaco"},{"label":"Mongolia"},{"label":"Montenegro"},{"label":"Montserrat"},{"label":"Morocco"},{"label":"Mozambique"},{"label":"Myanmar"},{"label":"Namibia"},{"label":"Nauru"},{"label":"Nepal"},{"label":"Netherlands"},{"label":"Netherlands Antilles"},{"label":"Nevis"},{"label":"New Caldonia"},{"label":"New Caledonia"},{"label":"New Guinea"},{"label":"New Hebrides"},{"label":"New Zealand"},{"label":"Nicaragua"},{"label":"Nicobar Islands"},{"label":"Niger"},{"label":"Nigeria"},{"label":"North Korea"},{"label":"Northern Ireland"},{"label":"Norway"},{"label":"Okinawa"},{"label":"Oman"},{"label":"Pagalu"},{"label":"Pakistan"},{"label":"Palau"},{"label":"Panama"},{"label":"Papua New Guinea"},{"label":"Paraguay"},{"label":"Peru"},{"label":"Petit Martinique"},{"label":"Philippines"},{"label":"Pitcairn Island"},{"label":"Poland"},{"label":"Portugal"},{"label":"Qatar"},{"label":"Republic of Congo"},{"label":"Republic of Georgia"},{"label":"Republic of Macedonia"},{"label":"Reunion"},{"label":"Romania"},{"label":"Russia"},{"label":"Rwanda"},{"label":"Ryukyu Islands"},{"label":"Saba"},{"label":"Samoa"},{"label":"San Marino"},{"label":"Sao Tome and Principe"},{"label":"Sardinia"},{"label":"Saudi Arabia"},{"label":"Scotland"},{"label":"Senegal"},{"label":"Serbia"},{"label":"Serbia and Montenegro"},{"label":"Seychelles"},{"label":"Sicily"},{"label":"Sierra Leone"},{"label":"Singapore"},{"label":"Slovak Republic"},{"label":"Slovakia"},{"label":"Slovenia"},{"label":"Solomon Islands"},{"label":"Somalia"},{"label":"Somaliland"},{"label":"Sombrero"},{"label":"South Africa"},{"label":"South Korea"},{"label":"Southwest Africa"},{"label":"Spain"},{"label":"Spanish Sahara"},{"label":"Sri Lanka"},{"label":"St Christopher"},{"label":"St Eustatius"},{"label":"St Helena"},{"label":"St Kitts"},{"label":"St Lucia"},{"label":"St Maarten"},{"label":"St Martin"},{"label":"St Pierre"},{"label":"St Vincent"},{"label":"Sudan"},{"label":"Sumatra"},{"label":"Suriname"},{"label":"Swaziland"},{"label":"Sweden"},{"label":"Switzerland"},{"label":"Syria"},{"label":"Tahiti"},{"label":"Taiwan"},{"label":"Tajikistan"},{"label":"Tanzania"},{"label":"Thailand"},{"label":"Timor"},{"label":"Togo"},{"label":"Tonga"},{"label":"Tortola"},{"label":"Trinidad and Tobago"},{"label":"Tristan da Cunha"},{"label":"Tunisia"},{"label":"Turkey"},{"label":"Turkmenistan"},{"label":"Turks and Caicos Is"},{"label":"Turks Islands"},{"label":"Tuvalu"},{"label":"Uganda"},{"label":"Ukraine"},{"label":"United Arab Emirates"},{"label":"United Kingdom"},{"label":"United States of America"},{"label":"Upper Volta"},{"label":"Uruguay"},{"label":"Uzbekistan"},{"label":"Vanuatu"},{"label":"Vatican City"},{"label":"Venezuela"},{"label":"Vietnam"},{"label":"Wake Island"},{"label":"Wales"},{"label":"Wallis and Futuna Is"},{"label":"Walvis Bay"},{"label":"Western Sahara"},{"label":"Western Samoa"},{"label":"Yemen"},{"label":"Yemen, Republic of"},{"label":"Zaire"},{"label":"Zambia"},{"label":"Zimbabwe"},{"label":"Cote D&apos;Ivoire"},{"label":"Rep of South Sudan"},{"label":"Kosovo"},{"label":"Alderney"},{"label":"Ascension"},{"label":"Brunei Darussalam"},{"label":"Guernsey"},{"label":"Ivory Coast"},{"label":"Jersey"},{"label":"Kowloon"},{"label":"Niue"},{"label":"Redonda"},{"label":"Santa Cruz Islands"},{"label":"Sark"},{"label":"St Bartholomew"},{"label":"St Christopher - Nevis"},{"label":"St Pierre and Miquelon"},{"label":"St Vincent - Grenadine"},{"label":"Syrian Arab Republic"},{"label":"Timor-Leste"},{"label":"Eswatini"}];
+
+
+
+
+const AddressTemplate = ({ label, labelSlotted, description, descriptionSlotted, fieldId, error, disabled, context, sx }) => {
+
+  // The native JSON objects may not be properly sorted (such as Countries, which has more appended to the end out of order),
+  // so sort them alphabetically.
+  
+  Countries.sort(function(a, b) {
+    return a.label.localeCompare(b.label);
+  });
+
+  return ` 
+    <cbp-form-field group
+      ${label ? `label="${label}"` : ''}
+      ${description ? `description="${description}"` : ''}
+      ${fieldId ? `field-id="${fieldId}"` : ''}
+      ${disabled ? `disabled` : ''}
+      ${error ? `error` : ''}
+      ${context && context != 'light-inverts' ? `context="${context}"` : ''}
+      ${sx ? `sx='${JSON.stringify(sx)}'` : ''}
+    >
+      ${ labelSlotted ? `<span slot="cbp-form-field-label">${labelSlotted}</span>` : ''}
+      ${ descriptionSlotted ? `<span slot="cbp-form-field-description">${descriptionSlotted}</span>` : ''}
+
+      <cbp-flex 
+        wrap="wrap"
+        gap="0 var(--cbp-space-4x)"
+        breakpoint="22.5rem"
+      >
+
+        <cbp-flex-item flex-grow="1" flex-basis="25ch">
+          <cbp-form-field
+            label="Street Address"
+          >
+            <input name="address1" type="text" />
+          </cbp-form-field>
+        </cbp-flex-item>
+
+        <cbp-flex-item flex-grow="1" flex-basis="25ch">
+          <cbp-form-field
+            label="Apartment, Suite, Building, Etc."
+          >
+            <input name="address2" type="text" />
+          </cbp-form-field>
+        </cbp-flex-item>
+
+        <cbp-flex-item flex-grow="1" flex-basis="25ch">
+          <cbp-form-field
+            label="Line 3"
+          >
+            <input name="address3" type="text" />
+          </cbp-form-field>
+        </cbp-flex-item>
+
+        <cbp-flex-item flex-grow="1" flex-basis="25ch">
+          <cbp-form-field
+            label="Line 4"
+          >
+            <input name="address4" type="text" />
+          </cbp-form-field>
+        </cbp-flex-item>
+
+        <cbp-flex-item flex-grow="1" flex-basis="25ch">
+          <cbp-form-field
+            label="City/Town"
+          >
+            <input name="city" type="text" />
+          </cbp-form-field>
+        </cbp-flex-item>
+
+        <cbp-flex-item flex-grow="1" flex-basis="25ch">
+          <cbp-form-field
+            label="State/Province/Region"
+          >
+            <input name="state" type="text" />
+          </cbp-form-field>
+        </cbp-flex-item>
+
+        <cbp-flex-item flex-grow="0" flex-basis="15ch">
+          <cbp-form-field
+            label="ZIP/Postal Code"
+          >
+            <input name="postalcode" type="text" />
+          </cbp-form-field>
+        </cbp-flex-item>
+
+        <cbp-flex-item flex-grow="2" flex-basis="20ch">
+          <cbp-form-field
+            label="Country"
+          >
+            <cbp-dropdown name="country" items='${JSON.stringify(Countries)}' />
+          </cbp-form-field>
+        </cbp-flex-item>
+
+      </cbp-flex>
+    </cbp-form-field>
+  `;
+};
+
+export const Address: any = AddressTemplate.bind({});
+Address.args = {
+  label:"Insert Group Label",
+  description: "Required.",
+};
+
 
 const Prefix='[{"label":"Attorney","value":"Attorney"},{"label":"Coach","value":"Coach"},{"label":"Dr.","value":"Dr."},{"label":"Father","value":"Father"},{"label":"Governor","value":"Governor"},{"label":"Honorable","value":"Honorable"},{"label":"Officer","value":"Officer"},{"label":"Master","value":"Master"},{"label":"Miss","value":"Miss"},{"label":"Mr.","value":"Mr."},{"label":"Mrs","value":"Mrs."},{"label":"Ms.","value":"Ms"},{"label":"President","value":"President"},{"label":"Professor","value":"Professor"},{"label":"Reverend","value":"Reverend"}]';
 const Suffix='[{"label":"I","value":"First"},{"label":"II","value":"Second"},{"label":"III","value":"Third"},{"label":"IV","value":"Fourth"},{"label":"IX","value":"Ninth"},{"label":"JR","value":"Junior"},{"label":"SR","value":"Senior"},{"label":"V","value":"Fifth"},{"label":"VI","value":"Sixth"},{"label":"VII","value":"Seventh"},{"label":"VIII","value":"Eighth"},{"label":"X","value":"Tenth"},{"label":"XI","value":"Eleventh"},{"label":"XII","value":"Twelfth"},{"label":"XIII","value":"Thirteenth"},{"label":"XIV","value":"Fourteenth"},{"label":"XV","value":"Fifteenth"},{"label":"XVI","value":"Sixteenth"},{"label":"XVII","value":"Seventeenth"}]';
@@ -62,7 +171,7 @@ const FullNameTemplate = ({ label, labelSlotted, description, descriptionSlotted
       <cbp-flex 
         wrap="wrap"
         gap="0 var(--cbp-space-4x)"
-        breakpoint="20rem"
+        breakpoint="22.5rem"
       >
 
         <cbp-flex-item
@@ -154,7 +263,7 @@ const PhoneTemplate = ({ label, labelSlotted, description, descriptionSlotted, f
       <cbp-flex 
         wrap="wrap"
         gap="0 var(--cbp-space-4x)"
-        breakpoint="20rem"
+        breakpoint="22.5rem"
       >
         <cbp-flex-item flex-basis="10rem" flex-shrink="0">
           <cbp-form-field label="Country Code">
@@ -184,14 +293,8 @@ Phone.args = {
 
 
 
-const GroupAsDividerTemplate = ({ label, labelSlotted, description, descriptionSlotted, fieldId, error, disabled, context, sx }) => {
+const GroupAsHeadingTemplate = ({ label, labelSlotted, description, descriptionSlotted, fieldId, error, disabled, context, sx }) => {
   return ` 
-    <cbp-typography tag="h3" divider="fill">
-      <cbp-icon name="user" size="1.5rem" sx='{"margin-inline-end":"var(--cbp-space-3x)"}'></cbp-icon>Applicant Information
-    </cbp-typography>
-
-    <br />
-
     <cbp-form-field group
       ${label ? `label="${label}"` : ''}
       ${description ? `description="${description}"` : ''}
@@ -207,7 +310,7 @@ const GroupAsDividerTemplate = ({ label, labelSlotted, description, descriptionS
       <cbp-flex 
         wrap="wrap"
         gap="0 var(--cbp-space-4x)"
-        breakpoint="20rem"
+        breakpoint="22.5rem"
       >
 
         <cbp-flex-item
@@ -266,7 +369,14 @@ const GroupAsDividerTemplate = ({ label, labelSlotted, description, descriptionS
   `;
 };
 
-export const GroupAsDivider: any = GroupAsDividerTemplate.bind({});
-GroupAsDivider.args = {
-  labelSlotted: `<cbp-typography tag="h3" divider="fill"><cbp-icon name="user" size="1.5rem" sx='{"margin-inline-end":"var(--cbp-space-3x)"}'></cbp-icon>Applicant Information</cbp-typography>`,
+export const GroupAsHeadingFill: any = GroupAsHeadingTemplate.bind({});
+GroupAsHeadingFill.storyName = "Group as Heading (Fill)";
+GroupAsHeadingFill.args = {
+  labelSlotted: `<cbp-typography tag="h3" divider="fill" sx='{"margin-block-end":"var(--cbp-space-2x)"}'><cbp-icon name="user" size="1.5rem" sx='{"margin-inline-end":"var(--cbp-space-3x)"}'></cbp-icon>Applicant Information</cbp-typography>`,
+};
+
+export const GroupAsHeadingUnderline: any = GroupAsHeadingTemplate.bind({});
+GroupAsHeadingUnderline.storyName = "Group as Heading (Underline)";
+GroupAsHeadingUnderline.args = {
+  labelSlotted: `<cbp-typography tag="h3" divider="underline" sx='{"margin-block-end":"var(--cbp-space-2x)"}'><cbp-icon name="user" size="1.5rem" sx='{"margin-inline-end":"var(--cbp-space-3x)"}'></cbp-icon>Applicant Information</cbp-typography>`,
 };
