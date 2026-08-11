a71afe6ab1c95a1224aeca8b428b6f3962aaec49
bagrub <barrettgrubbs@gmail.com>
2026-08-11 09:57:19 -0400

    WIP for standup sync

--- packages/web-components/src/components/cbp-link/cbp-link.stories.tsx
+++ packages/web-components/src/components/cbp-link/cbp-link.stories.tsx
@@ -71,17 +71,21 @@ export default {
   },
 };
 
+// Replace placeholder href="#" with unique URLs that do not show as "visited" (a common problem when using # or self-referencing URLs);
+// Should also use preventDefault() to avoid navigation to a broken link.
+function generateUnvisitedLink(href) {
+  if(href == '#') return '?' + (Math.random() + 1).toString(26).slice(2, 7);
+  else return href;
+}
+
+
 const Template = ({ label, withIcon, inText, href, rel, target, download, downloadFileName, language, shortcutKey, accessibilityText, disabled, context, sx }) => {
  
     setTimeout(() => {
       let anchors = document.querySelectorAll('cbp-link a');
       anchors.forEach(anchor => {
         // only prevent default for navigation
-        if(!anchor.hasAttribute('download')) anchor.addEventListener('click', function(e) {
-          e.preventDefault();
-          const target = e.target as HTMLElement
-          target.parentElement.classList.remove("initialLoad");
-        })
+        if(!anchor.hasAttribute('download')) anchor.addEventListener('click', function(e) {e.preventDefault();})
       });
     }, 500);
     
@@ -89,8 +93,7 @@ const Template = ({ label, withIcon, inText, href, rel, target, download, downlo
   return `
     ${ inText ? `<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. ` : ''}
     <cbp-link
-      class="initialLoad"
-      ${href ? `href="${href}"` : ''}
+      ${href ? `href="${generateUnvisitedLink(href)}"` : ''}
       ${rel ? `rel="${rel}"` : ''}
       ${target ? `target="${target}"` : ''}
       ${download ? `download${downloadFileName ? `="${downloadFileName}"` : ''}` : ''}
@@ -106,7 +109,6 @@ const Template = ({ label, withIcon, inText, href, rel, target, download, downlo
       ${withIcon == 'after' ? `<cbp-icon name="up-right-from-square"></cbp-icon>` : ''}
     </cbp-link>
     ${ inText ? ` Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>` : ''}
-    <style> .initialLoad a:visited:not(:hover):not(:active):not(:focus){color: var(--cbp-link-color)} </style>
   `;
 };
 
