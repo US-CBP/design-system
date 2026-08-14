5358196f19891ceda123a86f2fd6b699a40395a5
Doug Gibson <email@dgibson.net>
2026-08-14 09:37:48 -0400

    More improvements to menu via menu-item, added custom menuItemClick event.

--- packages/web-components/src/components/cbp-menu-item/cbp-menu-item.tsx
+++ packages/web-components/src/components/cbp-menu-item/cbp-menu-item.tsx
@@ -1,4 +1,4 @@
-import { Component, Element, Prop, Host, h } from '@stencil/core';
+import { Component, Element, Prop, Event, EventEmitter, Host, h } from '@stencil/core';
 import { setCSSProps } from '../../utils/utils';
 
 /**
@@ -12,7 +12,7 @@ import { setCSSProps } from '../../utils/utils';
 })
 export class CbpMenuItem {
 
-  private menuItem: HTMLElement;
+  private menuItem: HTMLAnchorElement | HTMLButtonElement;
 
   @Element() private host: HTMLElement;
 
@@ -27,6 +27,35 @@ export class CbpMenuItem {
     this.host.slot="cbp-menu-items"; // auto-slot the menu items
   }
 
+  /** 
+   * A custom event emitted when a menu item is clicked/activated.
+   */
+  @Event() menuItemClick: EventEmitter;
+  handleClick(e) {
+    const { target } = e;
+    const label = this.menuItem.innerText;
+  
+    this.menuItemClick.emit({
+      host: this.host,
+      nativeElement: this.menuItem,
+      nativeEvent: e,
+      target: target,
+      label: label,
+      value: this.menuItem.getAttribute('value'),
+    });
+  }
+
+  // Normalize link/button behavior as menu items and prevent space from scrolling the page
+  private handleKeyPress(e){
+    const { key } = e;
+    const activationKeys=['Enter',' '];
+    // For space/enter, manually trigger the click event.
+    if(activationKeys.includes(key)) {
+      e.preventDefault();
+      this.menuItem.click();
+    }
+  }
+
   componentWillLoad() {
     setCSSProps(this.host, {
       "--cpb-menu-item-indent": this.indentLevel > 0 ? `var(--cbp-space-${this.indentLevel * 2}x)` : "0px"
@@ -34,9 +63,13 @@ export class CbpMenuItem {
   }
 
   componentDidLoad() {
+    // The interactive element acts as the actual menu item
     this.menuItem = this.host.querySelector('a,button');
     this.menuItem?.setAttribute('role','menuitem');
     this.menuItem?.setAttribute('tabindex', '-1');
+    // Handle keydown and clicks on the slotted menu controls
+    this.menuItem?.addEventListener('keydown', e => this.handleKeyPress(e));
+    this.menuItem?.addEventListener('click', e => this.handleClick(e));
   }
 
   render() {
@@ -46,5 +79,4 @@ export class CbpMenuItem {
       </Host>
     );
   }
-
 }
