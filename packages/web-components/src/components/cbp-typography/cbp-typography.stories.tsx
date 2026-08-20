2f8ea0435ec3c89f7c4812af9633ea165725c26a
bagrub <barrettgrubbs@gmail.com>
2026-08-20 15:49:18 -0400

    update border radius tokens

--- packages/web-components/src/components/cbp-typography/cbp-typography.stories.tsx
+++ packages/web-components/src/components/cbp-typography/cbp-typography.stories.tsx
@@ -7,6 +7,9 @@ export default {
       description: 'The text content wrapped by the specified semantic tag and styles.',
       control: 'text',
     },
+    withIcon: {
+      control: 'boolean',
+    },
     tag: {
       control: 'select',
       description: 'The semantic element wrapping the content.',
@@ -33,7 +36,25 @@ export default {
   },
 };
 
-const Template = ({ text, tag, variant, size, lineheight, fontweight, divider, context, sx }) => {
+function generateIcon(variant){
+  switch (variant){
+    case 'heading-xxl':
+      return `<cbp-icon size='1.75rem' name="landmark" sx='{"margin-inline-end":"0.5rem"}'></cbp-icon>`
+    case 'heading-xl':
+      return `<cbp-icon size='1.5rem' name="landmark" sx='{"margin-inline-end":"0.5rem"}'></cbp-icon>`
+    case 'heading-lg':
+      return `<cbp-icon size='1.25rem' name="landmark" sx='{"margin-inline-end":"0.5rem"}'></cbp-icon>`
+    case 'heading-md':
+    case 'heading-sm':
+      return `<cbp-icon size='1rem' name="landmark" sx='{"margin-inline-end":"0.5rem"}'></cbp-icon>`
+    case 'heading-xs':
+      return `<cbp-icon size='0.75rem' name="landmark" sx='{"margin-inline-end":"0.25rem"}'></cbp-icon>`
+    default:
+      return `<cbp-icon size='0.875rem' name="landmark" sx='{"margin-inline-end":"0.25rem"}'></cbp-icon>`
+  }
+}
+
+const Template = ({ text, withIcon, tag, variant, size, lineheight, fontweight, divider, context, sx }) => {
   return ` 
     <cbp-typography
       ${tag ? `tag="${tag}"` : ''}
@@ -45,6 +66,7 @@ const Template = ({ text, tag, variant, size, lineheight, fontweight, divider, c
       ${context && context != 'light-inverts' ? `context="${context}"` : ''}
       ${sx ? `sx='${JSON.stringify(sx)}'` : ''}
     >
+      ${withIcon ? generateIcon(variant) : ''}
       ${text}
     </cbp-typography>
   `;
@@ -77,7 +99,7 @@ Typography.argTypes = {
 
 
 // TechDebt: This can be more efficiently achieved by looping over an array of all variants; refactor when we add the additional variants/tokens.
-const AllStyles = ({ text, tag, divider,  context, sx }) => {
+const AllStyles = ({ text, withIcon, tag, divider,  context, sx }) => {
   
   var variants = [ 'heading-xxl', 'heading-xl', 'heading-lg', 'heading-md', 'heading-sm', 'heading-xs', 'body-text', 'subhead'];;
   var htmlVariant = ``;
@@ -91,7 +113,8 @@ const AllStyles = ({ text, tag, divider,  context, sx }) => {
         ${divider != 'none' ? `divider="${divider}"` : ''}
         ${context && context != 'light-inverts' ? `context="${context}"` : ''}
         ${sx ? `sx='${JSON.stringify(sx)}'` : ''}
-      >
+      >      
+        ${withIcon ? generateIcon(variants[y]) : ''}
         ${text} (variant=${variants[y]})
       </cbp-typography>
     `
